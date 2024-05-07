5ad2a6b1ca170beffae4df2946614ef8d4007ca3
sadra <sadrataghavi1383@gmail.com>
2024-05-07 18:14:03 +0330

    feat(hero): init

--- app/pages/home/index.tsx
+++ app/pages/home/index.tsx
@@ -3,7 +3,11 @@ import { Component } from 'solid-js'
 import './style/home.scss'
 
 const Home: Component<{}> = props => {
-    return <main class='home'></main>
+    return (
+        <main class='home'>
+            <section class='hero-container'></section>
+        </main>
+    )
 }
 
 export default Home
