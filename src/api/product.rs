400d7162a18bc776d3f9985bde975fe7fc5dfd36
007 <dr007cc@gmail.com>
2024-05-08 13:35:26 +0330

    feat: init product api

--- src/api/product.rs
+++ src/api/product.rs
@@ -1,20 +1,16 @@
-use actix_web::web::{Data, Json, Query};
-use actix_web::{get, post, Scope};
-use serde::Deserialize;
-use utoipa::{OpenApi, ToSchema};
+use actix_web::web::Json;
+use actix_web::{get, Scope};
+use utoipa::OpenApi;
 
-use crate::config::Config;
+use crate::config::{config, Product};
 use crate::docs::UpdatePaths;
-use crate::general::{general_get, general_set};
-use crate::models::{ListInput, Offer, Product, Response, User};
-use crate::utils::sql_unwrap;
-use crate::AppState;
+use crate::models::Response;
 
 #[derive(OpenApi)]
 #[openapi(
     tags((name = "api::product")),
-    paths(product_list, product_get, offer_list, offer_add),
-    components(schemas(Offer, OfferAddBody)),
+    paths(products),
+    components(schemas(Product)),
     servers((url = "/products")),
     modifiers(&UpdatePaths)
 )]
@@ -22,132 +18,16 @@ pub struct Doc;
 
 #[utoipa::path(
     get,
-    params(("page" = u32, Query, example = 0)),
     responses(
         (status = 200, body = Vec<Product>)
     )
 )]
 /// Product List
 #[get("/")]
-async fn product_list(
-    query: Query<ListInput>, state: Data<AppState>,
-) -> Response<Vec<Product>> {
-    let offset = i64::from(query.page) * 30;
-
-    let products = sql_unwrap(
-        sqlx::query_as! {
-            Product,
-            "select * from products limit 30 offset ?",
-            offset
-        }
-        .fetch_all(&state.sql)
-        .await,
-    )?;
-
-    Ok(Json(products))
-}
-
-#[utoipa::path(
-    get,
-    params(("id" = i64, Path,)),
-    responses(
-        (status = 200, body = Product)
-    )
-)]
-/// Product Get
-#[get("/{id}/")]
-async fn product_get(_: User, product: Product) -> Json<Product> {
-    Json(product)
-}
-
-#[utoipa::path(
-    get,
-    params(
-        ("id" = i64, Path,),
-        ("page" = u32, Query,),
-    ),
-    responses(
-        (status = 200, body = Vec<Offer>)
-    )
-)]
-/// Product Offers List
-#[get("/{id}/offers/")]
-async fn offer_list(
-    _: User, product: Product, query: Query<ListInput>, state: Data<AppState>,
-) -> Response<Vec<Offer>> {
-    let offset = i64::from(query.page) * 30;
-
-    let offers = sql_unwrap(
-        sqlx::query_as! {
-            Offer,
-            "select * from offers where product = ? limit 30 offset ?",
-            product.id, offset
-        }
-        .fetch_all(&state.sql)
-        .await,
-    )?;
-
-    Ok(Json(offers))
-}
-
-#[derive(Deserialize, ToSchema)]
-struct OfferAddBody {
-    amount: u32,
-}
-
-#[utoipa::path(
-    post,
-    params(("id" = i64, Path,)),
-    responses(
-        (status = 200, body = Offer)
-    )
-)]
-/// Product Add Offer
-#[post("/{id}/offers/")]
-async fn offer_add(
-    user: User, product: Product, body: Json<OfferAddBody>,
-    state: Data<AppState>,
-) -> Response<Offer> {
-    let now = crate::utils::now();
-    let amount: i64 = i64::from(body.amount);
-
-    let offer_tax = ((body.amount as f32 / 100.0) * Config::OFFER_TAX)
-        .max(Config::OFFER_TAX_MAX);
-
-    let mut general = general_get(&state.sql).await?;
-    general.available_money += offer_tax as i64;
-    general_set(&state.sql, &general).await?;
-
-    // give back the last offer money aka unhold it.
-    // then take the offer_tax and put amount to hold
-    // if amount + offer_tax is not in users wallet
-    // then make the user pay that amount
-    // wallet - (amount + offer_tax)
-
-    let result = sql_unwrap(
-        sqlx::query_as! {
-            Offer,
-            "insert into offers(product, user, amount, timestamp)
-            values(?, ?, ?, ?)",
-            product.id, user.id, amount, now
-        }
-        .execute(&state.sql)
-        .await,
-    )?;
-
-    Ok(Json(Offer {
-        id: result.last_insert_rowid(),
-        product: product.id,
-        user: Some(user.id),
-        amount,
-        timestamp: now,
-    }))
+async fn products() -> Response<Vec<Product>> {
+    Ok(Json(config().products.clone()))
 }
 
 pub fn router() -> Scope {
-    Scope::new("/products")
-        .service(product_list)
-        .service(product_get)
-        .service(offer_list)
-        .service(offer_add)
+    Scope::new("/products").service(products)
 }
--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ async fn openapi() -> impl Responder {
     let mut doc = ApiDoc::openapi();
     doc.merge(api::user::ApiUserDoc::openapi());
     doc.merge(api::verification::ApiVerificationDoc::openapi());
-    // doc.merge(api::product::Doc::openapi());
+    doc.merge(api::product::Doc::openapi());
 
     let mut admin_doc = ApiDoc::openapi();
     // admin_doc.merge(admin::user::Doc::openapi());
@@ -104,7 +104,7 @@ async fn main() -> std::io::Result<()> {
             .service(
                 scope("/api")
                     .service(api::user::router())
-                    // .service(api::product::router())
+                    .service(api::product::router())
                     .service(api::verification::verification)
                     // .service(
                     //     scope("/admin")
