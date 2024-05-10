3a27f71f149773d51a685b3870d53636a9296a9f
007 <dr007cc@gmail.com>
2024-05-11 00:37:26 +0330

    fix: debug deploy script

--- config/deploy.sh
+++ config/deploy.sh
@@ -3,7 +3,7 @@ SPACER="======================================"
 EG="🔷"
 
 cd /p/dream/
-export $(head -n 1 secrets.env | xargs)
+export $(head -n 1 .secrets.env | xargs)
 
 OLD_COMMIT=$(git rev-parse HEAD)
 
