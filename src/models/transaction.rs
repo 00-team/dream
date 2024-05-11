c686b470610ecdbe38bdae7c7ed428ed221dd18f
007 <dr007cc@gmail.com>
2024-05-11 13:14:56 +0330

    fix(api): add timestamp in transaction's

--- src/models/transaction.rs
+++ src/models/transaction.rs
@@ -34,6 +34,7 @@ pub struct Transaction {
     pub status: TransactionStatus,
     pub vendor: TransactionVendor,
     pub amount: i64,
+    pub timestamp: i64,
     pub vendor_order_id: Option<String>,
     pub vendor_track_id: Option<i64>,
     pub card: Option<String>,
