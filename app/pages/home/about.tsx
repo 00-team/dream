8da8e531b77429dd65ef9acc4f2bd8deb6c60653
sadra <sadrataghavi1383@gmail.com>
2024-05-11 12:21:08 +0330

    feat(home)

--- app/pages/home/about.tsx
+++ app/pages/home/about.tsx
@@ -1,6 +1,8 @@
 import { CheckIcon, CrossIcon } from 'icons/home'
 import { Component, onMount } from 'solid-js'
 
+import VanillaTilt from 'utils/tilt'
+
 import './style/about.scss'
 
 export const About: Component = props => {
@@ -51,6 +53,64 @@ export const About: Component = props => {
         observer.observe(aboutSection)
     })
 
+    // let mouseEnter = (
+    //     e: MouseEvent & {
+    //         currentTarget: HTMLDivElement
+    //         target: Element
+    //     }
+    // ) => {
+    //     const el = e.currentTarget
+
+    //     el.className += ' active'
+    // }
+
+    // let mouseMove = (
+    //     e: MouseEvent & {
+    //         currentTarget: HTMLDivElement
+    //         target: Element
+    //     }
+    // ) => {
+    //     e.stopPropagation()
+
+    //     const el = e.currentTarget
+    //     const pos = el.getBoundingClientRect()
+    //     const mx = e.clientX - pos.left - pos.width / 2
+    //     const my = e.clientY - pos.top - pos.height / 2
+
+    //     console.log('x:', mx)
+    //     console.log('y:', my)
+
+    //     el.style.transform = `rotate3d(${mx * -0.1}, ${my * -0.3}, 0, 12deg)`
+    // }
+    // let mouseLeave = (
+    //     e: MouseEvent & {
+    //         currentTarget: HTMLDivElement
+    //         target: Element
+    //     }
+    // ) => {
+    //     const el = e.currentTarget
+
+    //     let newClass = el.className.replace(' active', '')
+
+    //     el.className = newClass
+
+    //     el.style.transform = ''
+    // }
+
+    let Cards
+
+    onMount(() => {
+        Cards = document.querySelectorAll('div.card-wrapper')
+
+        VanillaTilt.init(Cards, {
+            perspective: 1400,
+            scale: 1,
+            gyroscopeMaxAngleY: 10,
+            gyroscopeMaxAngleX: 10,
+            max: 5,
+        })
+    })
+
     return (
         <section class='about-us'>
             <div class='header-texts'>
@@ -62,60 +122,74 @@ export const About: Component = props => {
             </div>
             <div class='cards-container'>
                 <div class='card  left'>
-                    <h5 class='title'>سرویس های دیگه</h5>
-                    <div class='card-details title'>
-                        <div class='detail'>
-                            <CrossIcon />
-                            با واسطه، قیمت بالاتر{' '}
-                        </div>
-                        <div class='detail'>
-                            <CrossIcon />
-                            اکانت ها کرکی{' '}
-                        </div>
-                        <div class='detail'>
-                            <CrossIcon />
-                            پشتیبانی ضعیف{' '}
-                        </div>
-                        <div class='detail'>
-                            <CrossIcon />
-                            سرعت انی در تحویل
-                        </div>
-                        <div class='detail'>
-                            <CrossIcon />
-                            درگاه های ناامن{' '}
-                        </div>
-                        <div class='detail'>
-                            <CrossIcon />
-                            پرداخت های فقط داخلی
+                    <div
+                        class='card-wrapper'
+                        // onMouseEnter={mouseEnter}
+                        // onmousemove={mouseMove}
+                        // onmouseleave={mouseLeave}
+                    >
+                        <h5 class='title'>سرویس های دیگه</h5>
+                        <div class='card-details title'>
+                            <div class='detail'>
+                                <CrossIcon />
+                                با واسطه، قیمت بالاتر{' '}
+                            </div>
+                            <div class='detail'>
+                                <CrossIcon />
+                                اکانت ها کرکی{' '}
+                            </div>
+                            <div class='detail'>
+                                <CrossIcon />
+                                پشتیبانی ضعیف{' '}
+                            </div>
+                            <div class='detail'>
+                                <CrossIcon />
+                                سرعت پایین در تحویل
+                            </div>
+                            <div class='detail'>
+                                <CrossIcon />
+                                درگاه های ناامن{' '}
+                            </div>
+                            <div class='detail'>
+                                <CrossIcon />
+                                پرداخت های فقط داخلی
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class='card right'>
-                    <h5 class='logo title_hero'>Dream Pay</h5>
-                    <div class='card-details title'>
-                        <div class='detail'>
-                            <CheckIcon />
-                            بدون واسط، کمترین قیمت{' '}
-                        </div>
-                        <div class='detail'>
-                            <CheckIcon />
-                            اکانت ها قانونی{' '}
-                        </div>
-                        <div class='detail'>
-                            <CheckIcon />
-                            پشتیبانی 24 / 7{' '}
-                        </div>
-                        <div class='detail'>
-                            <CheckIcon />
-                            سرعت انی در تحویل
-                        </div>
-                        <div class='detail'>
-                            <CheckIcon />
-                            پرداخت امن{' '}
-                        </div>
-                        <div class='detail'>
-                            <CheckIcon />
-                            پرداخت با کارت های خارجی{' '}
+                    <div
+                        class='card-wrapper'
+                        // onMouseEnter={mouseEnter}
+                        // onmousemove={mouseMove}
+                        // onmouseleave={mouseLeave}
+                    >
+                        <h5 class='logo title_hero'>Dream Pay</h5>
+                        <div class='card-details title'>
+                            <div class='detail'>
+                                <CheckIcon />
+                                بدون واسط، کمترین قیمت{' '}
+                            </div>
+                            <div class='detail'>
+                                <CheckIcon />
+                                اکانت ها قانونی{' '}
+                            </div>
+                            <div class='detail'>
+                                <CheckIcon />
+                                پشتیبانی 24 ساعته
+                            </div>
+                            <div class='detail'>
+                                <CheckIcon />
+                                سرعت انی در تحویل
+                            </div>
+                            <div class='detail'>
+                                <CheckIcon />
+                                پرداخت امن{' '}
+                            </div>
+                            <div class='detail'>
+                                <CheckIcon />
+                                پرداخت با کارت های خارجی{' '}
+                            </div>
                         </div>
                     </div>
                 </div>
