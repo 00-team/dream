78493017f25ca80ccce0b10531dd2df256924f33
007 <dr007cc@gmail.com>
2024-05-11 15:52:33 +0330

    feat(api.admin): add order_update api

--- src/admin/order.rs
+++ src/admin/order.rs
@@ -1,19 +1,18 @@
-use actix_web::web::{Data, Json, Path, Query};
-use actix_web::{get, Scope};
-use utoipa::OpenApi;
+use actix_web::web::{Data, Json, Query};
+use actix_web::{get, post, HttpResponse, Scope};
+use serde::Deserialize;
+use utoipa::{OpenApi, ToSchema};
 
 use crate::docs::UpdatePaths;
-use crate::models::order::Order;
-use crate::models::user::Admin;
-use crate::models::{ListInput, Response};
+use crate::models::order::{Order, OrderStatus};
+use crate::models::user::{Admin, User};
+use crate::models::{AppErr, AppErrBadRequest, ListInput, Response};
 use crate::AppState;
 
 #[derive(OpenApi)]
 #[openapi(
     tags((name = "admin::order")),
-    paths(
-        order_list, order_get
-    ),
+    paths(order_list, order_get, order_update),
     components(schemas()),
     servers((url = "/orders")),
     modifiers(&UpdatePaths)
@@ -23,9 +22,7 @@ pub struct ApiDoc;
 #[utoipa::path(
     get,
     params(("page" = u32, Query, example = 0)),
-    responses(
-        (status = 200, body = Vec<Order>)
-    )
+    responses((status = 200, body = Vec<Order>))
 )]
 /// List
 #[get("/")]
@@ -46,24 +43,70 @@ async fn order_list(
 #[utoipa::path(
     get,
     params(("id" = i64, Path, example = 1)),
-    responses(
-        (status = 200, body = Order)
-    )
+    responses((status = 200, body = Order))
 )]
 /// Get
 #[get("/{id}/")]
-async fn order_get(
-    _: Admin, path: Path<(i64,)>, state: Data<AppState>,
-) -> Response<Order> {
-    let order = sqlx::query_as! {
-        Order, "select * from orders where id = ?", path.0
-    }
-    .fetch_one(&state.sql)
-    .await?;
-
+async fn order_get(_: Admin, order: Order) -> Response<Order> {
     Ok(Json(order))
 }
 
+#[derive(Deserialize, ToSchema)]
+struct UpdateOrder {
+    status: OrderStatus,
+}
+
+#[utoipa::path(
+    post,
+    request_body = UpdateOrder,
+    params(("id" = i64, Path, example = 1)),
+    responses((status = 200))
+)]
+/// Update
+#[post("/{id}/")]
+async fn order_update(
+    _: Admin, order: Order, body: Json<UpdateOrder>, state: Data<AppState>,
+) -> Result<HttpResponse, AppErr> {
+    if order.status != OrderStatus::Wating {
+        return Err(AppErrBadRequest("cannot change this order's status"));
+    }
+
+    if body.status == OrderStatus::Wating {
+        return Err(AppErrBadRequest("no change"));
+    }
+
+    if body.status == OrderStatus::Refunded {
+        let user = sqlx::query_as! {
+            User,
+            "select * from users where id = ?",
+            order.user
+        }
+        .fetch_one(&state.sql)
+        .await?;
+
+        let wallet = user.wallet + order.price;
+
+        sqlx::query! {
+            "update users set wallet = ? where id = ?",
+            wallet, order.user
+        }
+        .execute(&state.sql)
+        .await?;
+    }
+
+    sqlx::query! {
+        "update orders set status = ? where id = ?",
+        body.status, order.id
+    }
+    .execute(&state.sql)
+    .await?;
+
+    Ok(HttpResponse::Ok().finish())
+}
+
 pub fn router() -> Scope {
-    Scope::new("/orders").service(order_list).service(order_get)
+    Scope::new("/orders")
+        .service(order_list)
+        .service(order_get)
+        .service(order_update)
 }
