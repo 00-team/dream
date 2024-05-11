ad5a0f8a8cffed0415cab057712e826729d1c42e
sadra <sadrataghavi1383@gmail.com>
2024-05-11 15:40:02 +0330

    feat(home): new section

---
+++ app/pages/home/services.tsx
@@ -0,0 +1,16 @@
+import { Component } from 'solid-js'
+
+import './style/services.scss'
+
+export const Services: Component = props => {
+    return (
+        <section class='services-container'>
+            <header class='section-header'>
+                <h3 class='title'>
+                    بهترین خدمات آنلاین را برای سرگرمی های خود کشف کنید{' '}
+                </h3>
+                <h4 class='title_small'>محصولات ما</h4>
+            </header>
+        </section>
+    )
+}
