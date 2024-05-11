b9a35ddd1d3d70ba3292bb17f385ae95dd85fef4
007 <dr007cc@gmail.com>
2024-05-11 15:51:44 +0330

    feat(api): impl FromRequest for Order model

--- src/models/order.rs
+++ src/models/order.rs
@@ -1,7 +1,13 @@
+use std::{future::Future, pin::Pin};
+
+use actix_web::{web::{Data, Path}, FromRequest, HttpRequest};
+use actix_web::dev::Payload;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
-use super::{sql_enum, JsonStr};
+use crate::AppState;
+
+use super::{sql_enum, AppErr, JsonStr};
 
 sql_enum! {
     pub enum OrderStatus {
@@ -29,4 +35,28 @@ pub struct Order {
     #[schema(value_type = OrderData)]
     pub data: JsonStr<OrderData>,
     pub timestamp: i64,
+}
+
+impl FromRequest for Order {
+    type Error = AppErr;
+    type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
+
+    fn from_request(req: &HttpRequest, _: &mut Payload) -> Self::Future {
+        let path = Path::<(i64,)>::extract(req);
+        let state = req.app_data::<Data<AppState>>().unwrap();
+        let pool = state.sql.clone();
+
+        Box::pin(async move {
+            let path = path.await?;
+            let order = sqlx::query_as! {
+                Order,
+                "select * from orders where id = ?",
+                path.0
+            }
+            .fetch_one(&pool)
+            .await?;
+
+            Ok(order)
+        })
+    }
 }
