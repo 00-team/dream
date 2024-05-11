2f1c3412fe59b62e10394043a29eca69c13b9d56
007 <dr007cc@gmail.com>
2024-05-12 01:27:39 +0330

    fix(admin): update orders

--- admin/index.tsx
+++ admin/index.tsx
@@ -1,9 +1,11 @@
-import { Route, Router } from '@solidjs/router'
+import { Navigate, Route, Router, redirect } from '@solidjs/router'
 import { createEffect, lazy } from 'solid-js'
 import { render } from 'solid-js/web'
 import { self } from 'store/self'
 
 import './style/index.scss'
+
+const Orders = lazy(() => import('./layout/orders'))
 
 const Root = () => {
     createEffect(() => {
@@ -14,7 +16,9 @@ const Root = () => {
 
     return (
         <Router base='/admin'>
-            <Route path='/' component={lazy(() => import('./layout/orders'))} />
+            <Route path='/' component={() => <Navigate href='/orders' />} />
+            <Route path='/orders' component={Orders} />
+            <Route path='/orders/:page' component={Orders} />
             <Route path='*' component={() => <span>not found</span>} />
         </Router>
     )
--- admin/layout/orders.tsx
+++ admin/layout/orders.tsx
@@ -1,5 +1,52 @@
+import { createStore } from 'solid-js/store'
 import './style/orders.scss'
+import { OrderModel } from 'models'
+import { useNavigate, useParams } from '@solidjs/router'
+import { createEffect } from 'solid-js'
+import { httpx } from 'shared'
 
 export default () => {
-    return <div class='orders-fnd'>orders</div>
+    const UP = useParams()
+    const navigate = useNavigate()
+
+    type State = {
+        orders: OrderModel[]
+        page: number
+    }
+
+    const [state, setState] = createStore<State>({ orders: [], page: 0 })
+
+    createEffect(() => {
+        let pid = parseInt(UP.page || '0')
+
+        if (isNaN(pid)) {
+            return navigate('/orders/')
+        }
+
+        setState({ page: pid })
+        fetch_orders(pid)
+    })
+
+    function fetch_orders(page: number) {
+        httpx({
+            url: '/api/admin/orders/',
+            params: { page },
+            method: 'GET',
+            onLoad(x) {
+                setState({ orders: x.response, page })
+            },
+        })
+    }
+
+    return (
+        <div class='orders-fnd'>
+            <div class='order-list'>
+                {state.orders.map(o => (
+                    <div class='order'>
+                        {o.kind} - {o.price}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
