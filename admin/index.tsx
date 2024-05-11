5eda9cad09e4d6f1b61e7d0237f93ce07b99139f
007 <dr007cc@gmail.com>
2024-05-11 20:04:09 +0330

    fix(admin): redirect if not logged in as admin

--- admin/index.tsx
+++ admin/index.tsx
@@ -1,7 +1,21 @@
+import { Route, Router } from '@solidjs/router'
+import { createEffect } from 'solid-js'
 import { render } from 'solid-js/web'
+import { self } from 'store/self'
 
-export const Root = () => {
-    return <h1>hi from admin</h1>
+const Root = () => {
+    createEffect(() => {
+        if ((import.meta.env.PROD && !self.loged_in) || !self.user.admin) {
+            location.replace('/login')
+        }
+    })
+
+    return (
+        <Router>
+            <Route path='/' component={() => <span>app</span>} />
+            <Route path='*' component={() => <span>not found</span>} />
+        </Router>
+    )
 }
 
 render(() => <Root />, document.getElementById('root'))
