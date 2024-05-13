1b97ca677d02f452f42d2353c719581d6d141bf9
007 <dr007cc@gmail.com>
2024-05-14 00:00:26 +0330

    feat: add backup timer

--- config/backup.sh
+++ config/backup.sh
@@ -3,6 +3,8 @@
 BASE_URL="https://api.telegram.org/bot$TELOXIDE_TOKEN"
 BASE_NAME=dream-db-$(date +%s).tgz.
 
+mkdir .dbd
+
 function send_file {
     url="$BASE_URL/sendDocument?chat_id=$TELOXIDE_CHAT_ID"
     curl -s -X POST $url -F "document=@$1" -F "caption=$1" -o /dev/null
@@ -14,9 +16,13 @@ function send_msg {
 }
 
 send_msg "Starting the backup $(date +'%F %T')"
-tar czvf - main.db | split -d -b 40MB - $BASE_NAME
+tar czvf - main.db | split -d -b 40MB - .dbd/$BASE_NAME
+cd .dbd
 for f in $BASE_NAME*; do
     send_file $f
     sleep 2
 done
 send_msg "End of Backup 🐧"
+cd ..
+rm -rf .dbd
+
