1807b8c987435026a19084ee7fe9fc91c554a245
sadra <sadrataghavi1383@gmail.com>
2024-05-17 22:41:23 +0330

    feat(customers)

--- app/pages/home/customers.tsx
+++ app/pages/home/customers.tsx
@@ -3,5 +3,11 @@ import { Component } from 'solid-js'
 import './style/customers.scss'
 
 export const Customers: Component = props => {
-    return <section class='customers'></section>
+    return (
+        <section class='customers'>
+            <header>
+                <h3 class='section_title head'>نظرات مشتری هامون رو ببین!</h3>
+            </header>
+        </section>
+    )
 }
