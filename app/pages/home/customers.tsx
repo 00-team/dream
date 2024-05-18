2a4f7be776e54142f57fae65390e9646b6bd78e4
sadra <sadrataghavi1383@gmail.com>
2024-05-18 14:23:24 +0330

    feat(customers): say my name... , genuis sadra?, you are goddam right

--- app/pages/home/customers.tsx
+++ app/pages/home/customers.tsx
@@ -2,9 +2,13 @@ import { Component, onMount } from 'solid-js'
 
 import './style/customers.scss'
 
+import AudioFile from 'static/audio/type.mp3'
+
 export const Customers: Component = props => {
     let section: HTMLElement
     let htmlWord: HTMLElement
+
+    let audio: HTMLAudioElement
 
     let wordCount = 0
 
@@ -13,6 +17,47 @@ export const Customers: Component = props => {
     const words = ['نظرات مشتری هامون رو ببین!']
 
     let sentence = ''
+
+    onMount(() => {
+        audio = new Audio(AudioFile)
+
+        section = document.querySelector<HTMLElement>('section.customers')
+        htmlWord = document.querySelector<HTMLElement>('span#type-effect')
+
+        document.onscroll = () => {
+            let top = section.getBoundingClientRect().top - innerHeight + 100
+
+            if (top <= 0) {
+                let currentScrollPosition = scrollY
+
+                if (currentScrollPosition > lastScrollPosition) {
+                    if (wordCount <= words[0].length + 1) {
+                        let PauseAudio
+
+                        clearTimeout(PauseAudio)
+
+                        if (!(audio.duration > 0 && !audio.paused)) {
+                            audio.play()
+                        }
+
+                        let letter = Math.floor(-top / 20) - 1
+
+                        if (letter === wordCount) return
+
+                        wordCount = letter
+
+                        typeMessage()
+
+                        PauseAudio = setTimeout(() => {
+                            audio.pause()
+                        }, 500)
+                    }
+                }
+
+                lastScrollPosition = currentScrollPosition
+            }
+        }
+    })
 
     function typeMessage() {
         if (!words[0][wordCount]) {
@@ -27,36 +72,7 @@ export const Customers: Component = props => {
 
         sentence += currentStr[currentLetter]
         htmlWord.innerHTML = sentence
-
-        console.log(sentence)
     }
-
-    onMount(() => {
-        section = document.querySelector<HTMLElement>('section.customers')
-        htmlWord = document.querySelector<HTMLElement>('span#type-effect')
-
-        document.onscroll = () => {
-            let top = section.getBoundingClientRect().top - innerHeight + 100
-
-            if (top <= 0) {
-                let currentScrollPosition = scrollY
-
-                if (currentScrollPosition > lastScrollPosition) {
-                    if (wordCount <= words[0].length + 1) {
-                        let letter = Math.floor(-top / 20) - 1
-
-                        if (letter === wordCount) return
-
-                        wordCount = letter
-
-                        typeMessage()
-                    }
-                }
-
-                lastScrollPosition = currentScrollPosition
-            }
-        }
-    })
 
     return (
         <section class='customers'>
