21a374d10b00fc174d0f01f621f40d1f040c5d5a
sadra <sadrataghavi1383@gmail.com>
2024-05-18 14:07:37 +0330

    feat(customers)

--- app/pages/home/customers.tsx
+++ app/pages/home/customers.tsx
@@ -3,43 +3,66 @@ import { Component, onMount } from 'solid-js'
 import './style/customers.scss'
 
 export const Customers: Component = props => {
+    let section: HTMLElement
     let htmlWord: HTMLElement
+
+    let wordCount = 0
+
+    let lastScrollPosition = scrollY
 
     const words = ['نظرات مشتری هامون رو ببین!']
 
-    onMount(() => {
-        htmlWord = document.querySelector<HTMLElement>('span#type-effect')
-
-        typeMessage()
-    })
-
-    let currentMessage = 0
+    let sentence = ''
 
     function typeMessage() {
-        if (!words[currentMessage]) {
-            currentMessage = 0
+        if (!words[0][wordCount]) {
+            return
         }
-        const currentStr = words[currentMessage]
-        currentStr.split('')
-        let part = ''
-        let currentLetter = 0
-        let int1 = setInterval(() => {
-            if (!currentStr[currentLetter]) {
-                currentMessage++
 
-                clearInterval(int1)
-            } else {
-                part += currentStr[currentLetter++]
-                htmlWord.innerHTML = part
-            }
-        }, 100)
+        const currentStr = words[0]
+
+        currentStr.split('')
+
+        let currentLetter = wordCount
+
+        sentence += currentStr[currentLetter]
+        htmlWord.innerHTML = sentence
+
+        console.log(sentence)
     }
+
+    onMount(() => {
+        section = document.querySelector<HTMLElement>('section.customers')
+        htmlWord = document.querySelector<HTMLElement>('span#type-effect')
+
+        document.onscroll = () => {
+            let top = section.getBoundingClientRect().top - innerHeight + 100
+
+            if (top <= 0) {
+                let currentScrollPosition = scrollY
+
+                if (currentScrollPosition > lastScrollPosition) {
+                    if (wordCount <= words[0].length + 1) {
+                        let letter = Math.floor(-top / 20) - 1
+
+                        if (letter === wordCount) return
+
+                        wordCount = letter
+
+                        typeMessage()
+                    }
+                }
+
+                lastScrollPosition = currentScrollPosition
+            }
+        }
+    })
 
     return (
         <section class='customers'>
             <header>
                 <h3 class='section_title head'>
-                    <span id='type-effect'>نظرات مشتری هامون رو dasdsa!</span>
+                    <span id='type-effect'></span>
                     <div class='cursor'></div>
                 </h3>
             </header>
