3a79e212d08360eb8aae9a61638fa7a219858a12
sadra <sadrataghavi1383@gmail.com>
2024-05-20 14:24:49 +0330

    feat(faq)

--- app/pages/home/faq.tsx
+++ app/pages/home/faq.tsx
@@ -1,9 +1,19 @@
+import { FaqIcon } from 'icons/home'
 import { Component } from 'solid-js'
 
 import './style/faq.scss'
 
 const Faq: Component = props => {
-    return <section class='faq-container'></section>
+    return (
+        <section class='faq-container'>
+            <header class='header'>
+                <FaqIcon size={50} />
+                <h3 class='section_title'>سوالات متداول</h3>
+                <FaqIcon class='reverse' size={50} />
+            </header>
+            <div class='faq-wrapper'></div>
+        </section>
+    )
 }
 
 export default Faq
