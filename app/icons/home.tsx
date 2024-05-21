a5e5b440149751346d9285e89687def8f850efe9
sadra <sadrataghavi1383@gmail.com>
2024-05-21 19:25:52 +0330

    feat(faq): icon

--- app/icons/home.tsx
+++ app/icons/home.tsx
@@ -64,3 +64,17 @@ export const FaqIcon = P => (
         <path d='M408 48H104a72.08 72.08 0 0 0-72 72v192a72.08 72.08 0 0 0 72 72h24v64a16 16 0 0 0 26.25 12.29L245.74 384H408a72.08 72.08 0 0 0 72-72V120a72.08 72.08 0 0 0-72-72zM160 248a32 32 0 1 1 32-32 32 32 0 0 1-32 32zm96 0a32 32 0 1 1 32-32 32 32 0 0 1-32 32zm96 0a32 32 0 1 1 32-32 32 32 0 0 1-32 32z'></path>
     </svg>
 )
+
+export const ArrowDownIcon = P => (
+    <svg
+        stroke='currentColor'
+        fill='currentColor'
+        stroke-width='0'
+        viewBox='0 0 512 512'
+        height={P.size || 25}
+        width={P.size || 25}
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path d='M256 294.1L383 167c9.4-9.4 24.6-9.4 33.9 0s9.3 24.6 0 34L273 345c-9.1 9.1-23.7 9.3-33.1.7L95 201.1c-4.7-4.7-7-10.9-7-17s2.3-12.3 7-17c9.4-9.4 24.6-9.4 33.9 0l127.1 127z'></path>
+    </svg>
+)
