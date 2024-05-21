88b4190b9ec4e125a489eacf30405ce4c9551110
sadra <sadrataghavi1383@gmail.com>
2024-05-21 21:14:16 +0330

    feat(home)

--- app/pages/home/index.tsx
+++ app/pages/home/index.tsx
@@ -9,7 +9,7 @@ import './style/home.scss'
 const Home: Component<{}> = props => {
     return (
         <main class='home'>
-            <section class='hero-container'>
+            <section class='hero-container' id='ero'>
                 <iframe src='https://my.spline.design/dreampay-0a78cdac709ee611db084ccb679701db/'></iframe>
             </section>
 
