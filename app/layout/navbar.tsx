8bb7d88f12ae6a636e98916b1f9e0ed9527978e7
sadra <sadrataghavi1383@gmail.com>
2024-05-22 12:47:27 +0330

    feat(nav)

--- app/layout/navbar.tsx
+++ app/layout/navbar.tsx
@@ -1,7 +1,32 @@
-import { Component } from 'solid-js'
+import { Component, onMount } from 'solid-js'
+
+import './style/navbar.scss'
+
+import logo from 'static/imgs/logo.png'
 
 const Navbar: Component = props => {
-    return <nav class='nav-container'></nav>
-}
+    let bigNav: HTMLElement
 
+    onMount(() => {
+        bigNav = document.querySelector<HTMLElement>('nav.nav-big-container')
+
+        document.addEventListener('scroll', () => {
+            if (scrollY >= 200) {
+                if (!bigNav.classList.contains('active')) {
+                    bigNav.className += ' active'
+                }
+            } else {
+                bigNav.className = 'nav-big-container'
+            }
+        })
+    })
+    return (
+        <>
+            <nav class='nav-big-container'>
+                <div class='nav-links'></div>
+                <img class='nav-logo' src={logo} alt='' />
+            </nav>
+        </>
+    )
+}
 export default Navbar
