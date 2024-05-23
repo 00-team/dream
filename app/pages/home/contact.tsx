782817db045a3fac548bb38b6515510bacf87a57
sadra <sadrataghavi1383@gmail.com>
2024-05-23 13:12:15 +0330

    feat(contact): init

---
+++ app/pages/home/contact.tsx
@@ -0,0 +1,10 @@
+import { Component } from "solid-js";
+
+import "./style/contact.scss"
+
+const Contact: Component = (props) => {
+  
+  return <section class="contact"></section>;
+};
+
+export default Contact
