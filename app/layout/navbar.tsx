861cd74d4c5e2407561f30760084bf41049ffa19
sadra <sadrataghavi1383@gmail.com>
2024-05-23 22:58:20 +0330

    feat(navbar)

--- app/layout/navbar.tsx
+++ app/layout/navbar.tsx
@@ -1,4 +1,4 @@
-import { Component, onMount } from 'solid-js'
+import { Component, createSignal, onMount } from 'solid-js'
 
 import './style/navbar.scss'
 
@@ -6,7 +6,15 @@ import { ArrowDownIcon, FaqIcon } from 'icons/home'
 import { HomeIcon, ProductsIcon, SupportIcon } from 'icons/navbar'
 import logo from 'static/imgs/logo.png'
 
+import appleMusicImg from 'static/imgs/apple-music.png'
+import CanvaImg from 'static/imgs/canva.png'
+import discordImg from 'static/imgs/discord.png'
+import spotifyImg from 'static/imgs/spotify.png'
+import youtubeImg from 'static/imgs/youtube.png'
+
 const Navbar: Component = props => {
+    const [dropdown, setDropdown] = createSignal(false)
+
     let bigNav: HTMLElement
 
     let navLinks: NodeListOf<HTMLElement>
@@ -57,10 +65,15 @@ const Navbar: Component = props => {
                         <HomeIcon />
                         خانه
                     </a>
-                    <a class='nav-link title_small'>
+                    <a
+                        class='nav-link title_small'
+                        onmouseenter={() => setDropdown(true)}
+                        onmouseleave={() => setDropdown(false)}
+                    >
                         <ProductsIcon />
                         محصولات
                         <ArrowDownIcon class='drop' />
+                        <DropDown show={dropdown()} />
                     </a>
                     <a class='nav-link title_small'>
                         <SupportIcon />
@@ -77,4 +90,46 @@ const Navbar: Component = props => {
         </>
     )
 }
+
+interface dropdownProps {
+    show: boolean
+}
+
+const DropDown: Component<dropdownProps> = P => {
+    return (
+        <div class='dropdown' classList={{ active: P.show }}>
+            <div class='drop-link title_smaller'>
+                <div class='holder icon'>
+                    <img src={discordImg} alt='' />
+                </div>
+                <div class='data'>دیسکورد</div>
+            </div>
+            <div class='drop-link title_smaller'>
+                <div class='holder icon'>
+                    <img src={spotifyImg} alt='' />
+                </div>
+                <div class='data'>اسپاتیفای</div>
+            </div>
+            <div class='drop-link title_smaller'>
+                <div class='holder icon'>
+                    <img src={CanvaImg} alt='' />
+                </div>
+                <div class='data'>کانوا</div>
+            </div>
+            <div class='drop-link title_smaller'>
+                <div class='holder icon'>
+                    <img src={youtubeImg} alt='' />
+                </div>
+                <div class='data'>یوتیوب</div>
+            </div>
+            <div class='drop-link title_smaller'>
+                <div class='holder icon'>
+                    <img src={appleMusicImg} alt='' />
+                </div>
+                <div class='data'>اپل موزیک</div>
+            </div>
+        </div>
+    )
+}
+
 export default Navbar
