d80e386e5794e5ea4e085fcf51ca2000b4cfe92e
sadra <sadrataghavi1383@gmail.com>
2024-05-23 13:54:41 +0330

    feat(contact)

--- app/pages/home/contact.tsx
+++ app/pages/home/contact.tsx
@@ -1,13 +1,44 @@
-import { Component } from 'solid-js'
+import { Component, onMount } from 'solid-js'
 
 import './style/contact.scss'
 
 const Contact: Component = props => {
+    let header: HTMLElement
+    let subheader: HTMLElement
+
+    onMount(() => {
+        header = document.querySelector('h4#contact-header')
+        subheader = document.querySelector('h5#contact-subheader')
+
+        document.addEventListener('scroll', () => {
+            let top = header.getBoundingClientRect().top - innerHeight
+
+            if (top <= 0) {
+                let val = Math.min(-top * 0.5, 450)
+
+                header.style.backgroundImage = `linear-gradient(
+                    90deg,
+                    transparent ${100 - (val - 10)}%,
+                    #000000 140%
+                )`
+                subheader.style.backgroundImage = `linear-gradient(
+                    90deg,
+                    transparent ${100 - (val - 10)}%,
+                    #000000 140%
+                )`
+            }
+        })
+    })
+
     return (
         <section class='contact'>
             <header class='contact-header'>
-                <h4 class='section_title'>جواب سوالتو پیدا نکردی؟</h4>
-                <h5 class='title_small subheader'>باما در ارتباط باش!</h5>
+                <h4 class='section_title' id='contact-header'>
+                    جواب سوالتو پیدا نکردی؟
+                </h4>
+                <h5 class='title_small subheader' id='contact-subheader'>
+                    باما در ارتباط باش!
+                </h5>
             </header>
         </section>
     )
