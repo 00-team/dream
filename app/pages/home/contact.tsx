e4b7ea070c19e4ae6ec76122ba1683e71577f404
sadra <sadrataghavi1383@gmail.com>
2024-05-23 13:31:10 +0330

    feat(contact):: style

--- app/pages/home/contact.tsx
+++ app/pages/home/contact.tsx
@@ -1,10 +1,16 @@
-import { Component } from "solid-js";
+import { Component } from 'solid-js'
 
-import "./style/contact.scss"
+import './style/contact.scss'
 
-const Contact: Component = (props) => {
-  
-  return <section class="contact"></section>;
-};
+const Contact: Component = props => {
+    return (
+        <section class='contact'>
+            <header class='contact-header'>
+                <h4 class='section_title'>جواب سوالتو پیدا نکردی؟</h4>
+                <h5 class='title_small subheader'>باما در ارتباط باش!</h5>
+            </header>
+        </section>
+    )
+}
 
 export default Contact
