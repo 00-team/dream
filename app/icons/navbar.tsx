692d7aa45d86b97759fe20e87dbec5e1bb493be5
sadra <sadrataghavi1383@gmail.com>
2024-05-25 23:41:37 +0330

    feat(nav)

--- app/icons/navbar.tsx
+++ app/icons/navbar.tsx
@@ -53,3 +53,19 @@ export const MenuIcon = P => (
         <path d='M3 4H21V6H3V4ZM9 11H21V13H9V11ZM3 18H21V20H3V18Z'></path>
     </svg>
 )
+
+export const AboutIcon = P => (
+    <svg
+        stroke='currentColor'
+        fill='currentColor'
+        stroke-width='0'
+        viewBox='0 0 24 24'
+        height={P.size || 25}
+        width={P.size || 25}
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path fill='none' d='M0 0h24v24H0z'></path>
+        <circle cx='12' cy='4' r='2'></circle>
+        <path d='M15.89 8.11C15.5 7.72 14.83 7 13.53 7h-2.54C8.24 6.99 6 4.75 6 2H4c0 3.16 2.11 5.84 5 6.71V22h2v-6h2v6h2V10.05L18.95 14l1.41-1.41-4.47-4.48z'></path>
+    </svg>
+)
