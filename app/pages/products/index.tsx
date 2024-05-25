c47cf700e908c2b134c1ca035ae0f1f88b0f1bc0
sadra <sadrataghavi1383@gmail.com>
2024-05-25 13:42:03 +0330

    feat(products): init

---
+++ app/pages/products/index.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'solid-js'
+
+import './style/products.scss'
+
+const Products: Component = props => {
+    return <main class='products'></main>
+}
+
+export default Products
