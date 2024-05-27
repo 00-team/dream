39cb114408d40bb439aab4d1e9f2b08888c7e0b4
sadra <sadrataghavi1383@gmail.com>
2024-05-27 17:11:25 +0330

    feat(products)

--- app/pages/products/index.tsx
+++ app/pages/products/index.tsx
@@ -1,20 +1,52 @@
-import { Component } from 'solid-js'
+import { Component, onMount } from 'solid-js'
 
 import './style/products.scss'
 
+import { CheckIcon } from 'icons/home'
 import discordbanner from 'static/imgs/banners/discord.jpg'
 import spotifybanner from 'static/imgs/banners/spotify.png'
 import tradingviewbanner from 'static/imgs/banners/tradingview.jpg'
-import { CheckIcon } from 'icons/home'
 
 const Products: Component = props => {
+    let cards: NodeListOf<HTMLElement>
+
+    onMount(() => {
+        cards = document.querySelectorAll('.product-card')
+
+        cards.forEach((card: HTMLElement) => {
+            card.onmousemove = e => {
+                const { currentTarget: target } = e
+
+                const rect = card.getBoundingClientRect()
+
+                let x = e.clientX - rect.left
+                let y = e.clientY - rect.top
+
+                card.style.setProperty('--mouse-x', `${x}px`)
+                card.style.setProperty('--mouse-y', `${y}px`)
+            }
+        })
+    })
+
     return (
         <main class='products'>
             <header class='products-header'></header>
             <div class='products-wrapper'>
-                <ProductCard title='دیسکورد' img={discordbanner} />
-                <ProductCard title='اسپاتیفای' img={spotifybanner} />
-                <ProductCard title='تریدینگ ویو' img={tradingviewbanner} />
+                <ProductCard
+                    product='discord'
+                    title='دیسکورد'
+                    img={discordbanner}
+                />
+                <ProductCard
+                    product='spotify'
+                    title='اسپاتیفای'
+                    img={spotifybanner}
+                />
+                <ProductCard
+                    product='tradingview'
+                    title='تریدینگ ویو'
+                    img={tradingviewbanner}
+                />
             </div>
         </main>
     )
@@ -23,13 +55,14 @@ const Products: Component = props => {
 interface ProductCardProps {
     img: string
     title: string
+    product: string
 }
 
 const options = ['تضمین اصل بودن', 'تحویل فوری']
 
 const ProductCard: Component<ProductCardProps> = P => {
     return (
-        <figure class='product-card'>
+        <figure class={`product-card ${P.product || ''}`}>
             <div class='img-wrapper'>
                 <img src={P.img} class='card-img' alt='' />
             </div>
