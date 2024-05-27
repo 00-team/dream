25b3fd9ca9cc2cafaf34a1b5acc502739e4d3efa
sadra <sadrataghavi1383@gmail.com>
2024-05-27 16:34:01 +0330

    feat(prodcts)

--- app/pages/products/index.tsx
+++ app/pages/products/index.tsx
@@ -5,6 +5,7 @@ import './style/products.scss'
 import discordbanner from 'static/imgs/banners/discord.jpg'
 import spotifybanner from 'static/imgs/banners/spotify.png'
 import tradingviewbanner from 'static/imgs/banners/tradingview.jpg'
+import { CheckIcon } from 'icons/home'
 
 const Products: Component = props => {
     return (
@@ -23,6 +24,9 @@ interface ProductCardProps {
     img: string
     title: string
 }
+
+const options = ['تضمین اصل بودن', 'تحویل فوری']
+
 const ProductCard: Component<ProductCardProps> = P => {
     return (
         <figure class='product-card'>
@@ -31,6 +35,17 @@ const ProductCard: Component<ProductCardProps> = P => {
             </div>
             <div class='card-title title_small'>
                 <span>{P.title}</span>
+            </div>
+
+            <div class='product-options'>
+                {options.map(text => {
+                    return (
+                        <div class='product-option description'>
+                            <CheckIcon />
+                            {text}
+                        </div>
+                    )
+                })}
             </div>
 
             <button class='card-buy title_smaller'>خرید</button>
