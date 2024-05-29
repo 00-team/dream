fb60d5f02dec78760ebaedada376a7da900d9c97
sadra <sadrataghavi1383@gmail.com>
2024-05-29 16:11:10 +0330

    feat(products)

--- app/pages/products/index.tsx
+++ app/pages/products/index.tsx
@@ -1,4 +1,4 @@
-import { Component, onMount } from 'solid-js'
+import { Component, createEffect, onCleanup, onMount } from 'solid-js'
 
 import './style/products.scss'
 
@@ -178,128 +178,142 @@ const ProductCard: Component<ProductCardProps> = P => {
 interface ProductPopUpProps {}
 
 const ProductPopUp: Component<ProductPopUpProps> = P => {
-    onMount(() => {
-        // @ts-ignore
-        particlesJS(
-            'particles-js',
+    let particles: HTMLElement
 
-            {
-                particles: {
-                    number: {
-                        value: 80,
-                        density: {
-                            enable: true,
-                            value_area: 800,
-                        },
-                    },
-                    color: {
-                        value: '#2a78bd',
-                    },
-                    shape: {
-                        type: 'circle',
-                        stroke: {
-                            width: 0,
-                            color: '#2a78bd',
-                        },
-                        polygon: {
-                            nb_sides: 5,
-                        },
-                        image: {
-                            src: tradingview,
-                            width: 100,
-                            height: 100,
-                        },
-                    },
-                    opacity: {
-                        value: 0.5,
-                        random: false,
-                        anim: {
-                            enable: false,
-                            speed: 1,
-                            opacity_min: 0.1,
-                            sync: false,
-                        },
-                    },
-                    size: {
-                        value: 5,
-                        random: true,
-                        anim: {
-                            enable: false,
-                            speed: 40,
-                            size_min: 0.1,
-                            sync: false,
-                        },
-                    },
-                    line_linked: {
-                        enable: true,
-                        distance: 150,
-                        color: '#2a78bd',
-                        opacity: 0.4,
-                        width: 1,
-                    },
-                    move: {
-                        enable: true,
-                        speed: 6,
-                        direction: 'none',
-                        random: false,
-                        straight: false,
-                        out_mode: 'out',
-                        attract: {
-                            enable: false,
-                            rotateX: 600,
-                            rotateY: 1200,
-                        },
-                    },
-                },
-                interactivity: {
-                    detect_on: 'canvas',
-                    events: {
-                        onhover: {
-                            enable: true,
-                            mode: 'repulse',
-                        },
-                        onclick: {
-                            enable: true,
-                            mode: 'push',
-                        },
-                        resize: true,
-                    },
-                    modes: {
-                        grab: {
-                            distance: 400,
-                            line_linked: {
-                                opacity: 1,
+    onMount(() => {
+        particles = document.getElementById('particles-js')
+    })
+
+    createEffect(() => {
+        if (popup.show) {
+            // @ts-ignore
+            particlesJS(
+                'particles-js',
+
+                {
+                    particles: {
+                        number: {
+                            value: 80,
+                            density: {
+                                enable: true,
+                                value_area: 800,
                             },
                         },
-                        bubble: {
-                            distance: 400,
-                            size: 40,
-                            duration: 2,
-                            opacity: 8,
-                            speed: 3,
+                        color: {
+                            value: '#2a78bd',
                         },
-                        repulse: {
-                            distance: 200,
+                        shape: {
+                            type: 'circle',
+                            stroke: {
+                                width: 0,
+                                color: '#2a78bd',
+                            },
+                            polygon: {
+                                nb_sides: 5,
+                            },
+                            image: {
+                                src: tradingview,
+                                width: 100,
+                                height: 100,
+                            },
                         },
-                        push: {
-                            particles_nb: 4,
+                        opacity: {
+                            value: 0.5,
+                            random: false,
+                            anim: {
+                                enable: false,
+                                speed: 1,
+                                opacity_min: 0.1,
+                                sync: false,
+                            },
                         },
-                        remove: {
-                            particles_nb: 2,
+                        size: {
+                            value: 5,
+                            random: true,
+                            anim: {
+                                enable: false,
+                                speed: 40,
+                                size_min: 0.1,
+                                sync: false,
+                            },
+                        },
+                        line_linked: {
+                            enable: true,
+                            distance: 150,
+                            color: '#2a78bd',
+                            opacity: 0.4,
+                            width: 1,
+                        },
+                        move: {
+                            enable: true,
+                            speed: 6,
+                            direction: 'none',
+                            random: false,
+                            straight: false,
+                            out_mode: 'out',
+                            attract: {
+                                enable: false,
+                                rotateX: 600,
+                                rotateY: 1200,
+                            },
                         },
                     },
-                },
-                retina_detect: true,
-                config_demo: {
-                    hide_card: false,
-                    background_color: '#b61924',
-                    background_image: '',
-                    background_position: '50% 50%',
-                    background_repeat: 'no-repeat',
-                    background_size: 'cover',
-                },
-            }
-        )
+                    interactivity: {
+                        detect_on: 'canvas',
+                        events: {
+                            onhover: {
+                                enable: true,
+                                mode: 'repulse',
+                            },
+                            onclick: {
+                                enable: true,
+                                mode: 'push',
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            grab: {
+                                distance: 400,
+                                line_linked: {
+                                    opacity: 1,
+                                },
+                            },
+                            bubble: {
+                                distance: 400,
+                                size: 40,
+                                duration: 2,
+                                opacity: 8,
+                                speed: 3,
+                            },
+                            repulse: {
+                                distance: 200,
+                            },
+                            push: {
+                                particles_nb: 4,
+                            },
+                            remove: {
+                                particles_nb: 2,
+                            },
+                        },
+                    },
+                    retina_detect: true,
+                    config_demo: {
+                        hide_card: false,
+                        background_color: '#b61924',
+                        background_image: '',
+                        background_position: '50% 50%',
+                        background_repeat: 'no-repeat',
+                        background_size: 'cover',
+                    },
+                }
+            )
+        } else {
+            particles.childNodes[0].remove()
+        }
+    })
+
+    onCleanup(() => {
+        particles.childNodes[0].remove()
     })
 
     return (
