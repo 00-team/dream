bd3314e1d647795a6dd210598949e28f51aadb36
007 <dr007cc@gmail.com>
2024-05-30 18:19:43 +0330

    feat(app): add user model

--- app/models.ts
+++ app/models.ts
@@ -6,3 +6,15 @@ export type Product = {
     color: string
     image: string
 }
+
+export type UserModel = {
+    id: number
+    admin: boolean
+    banned: boolean
+    in_hold: number
+    name: string | null
+    phone: string
+    photo: string | null
+    token: string
+    wallet: number
+}
