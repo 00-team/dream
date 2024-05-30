a8de79f5bd0a79e14d1a644e686f7a9ac4c83d04
sadra <sadrataghavi1383@gmail.com>
2024-05-30 17:13:40 +0330

    feat(login)

--- app/pages/auth/login.tsx
+++ app/pages/auth/login.tsx
@@ -1,9 +1,21 @@
 import { PhoneIcon } from 'icons/login'
 import { Component } from 'solid-js'
+import { createStore } from 'solid-js/store'
 
 import './style/login.scss'
 
+type loginState = {
+    stage: 'phone' | 'code'
+    phone: ''
+    code: ''
+}
+
 const Login: Component = props => {
+    const [login, setlogin] = createStore<loginState>({
+        stage: 'phone',
+        phone: '',
+        code: '',
+    })
     return (
         <main class='login'>
             <iframe src='https://my.spline.design/untitled-51a258c8798e70e51855f9dd800ffa1c/'></iframe>
