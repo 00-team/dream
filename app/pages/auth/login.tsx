0cb58af2fd34cac8ec029ecbaa9c1a5b2fba4215
sadra <sadrataghavi1383@gmail.com>
2024-05-30 17:10:44 +0330

    feat(login)

--- app/pages/auth/login.tsx
+++ app/pages/auth/login.tsx
@@ -1,3 +1,4 @@
+import { PhoneIcon } from 'icons/login'
 import { Component } from 'solid-js'
 
 import './style/login.scss'
@@ -6,6 +7,25 @@ const Login: Component = props => {
     return (
         <main class='login'>
             <iframe src='https://my.spline.design/untitled-51a258c8798e70e51855f9dd800ffa1c/'></iframe>
+            <div class='login-wrapper'>
+                <h2 class='title'>Dream Pay</h2>
+                <h3 class='title_small'>ورود</h3>
+
+                <div class='inp phone'>
+                    <h3 class='holder title_smaller'>
+                        <PhoneIcon />
+                        شماره تلفن
+                    </h3>
+                    <input
+                        type='number'
+                        inputMode={'numeric'}
+                        class='phone-inp title_small'
+                        placeholder='09123456789'
+                    />
+                </div>
+
+                <button class='title_smaller cta'>ارسال کد</button>
+            </div>
         </main>
     )
 }
