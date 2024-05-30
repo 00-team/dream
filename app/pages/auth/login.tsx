caa9b1cdab746f9d61012822c677c8efb669994b
sadra <sadrataghavi1383@gmail.com>
2024-05-30 16:41:19 +0330

    feat(login)

--- app/pages/auth/login.tsx
+++ app/pages/auth/login.tsx
@@ -3,7 +3,11 @@ import { Component } from 'solid-js'
 import './style/login.scss'
 
 const Login: Component = props => {
-    return <main class='login'></main>
+    return (
+        <main class='login'>
+            <iframe src='https://my.spline.design/untitled-51a258c8798e70e51855f9dd800ffa1c/'></iframe>
+        </main>
+    )
 }
 
 export default Login
