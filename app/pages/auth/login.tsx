617e9983b3e2eea8ad7a3dc2de15e761db0201f6
sadra <sadrataghavi1383@gmail.com>
2024-05-30 17:22:38 +0330

    feat(login): style

--- app/pages/auth/login.tsx
+++ app/pages/auth/login.tsx
@@ -6,16 +6,38 @@ import './style/login.scss'
 
 type loginState = {
     stage: 'phone' | 'code'
-    phone: ''
-    code: ''
+    phone: string
+    code: string
+    error: string
 }
+
+let phoneRegex = /^(0|09|09[0-9]{1,9})$/
 
 const Login: Component = props => {
     const [login, setlogin] = createStore<loginState>({
         stage: 'phone',
         phone: '',
         code: '',
+        error: '',
     })
+
+    function CheckPhone() {
+        if (login.phone.length !== 11)
+            return setlogin({ error: 'شماره تلفن خود را به درستی وارد کنید!' })
+
+        if (login.phone[0] !== '0')
+            return setlogin({
+                error: 'شماره تلفن خود را با پیش شماره 0 وارد کنید',
+            })
+
+        if (!phoneRegex.test(login.phone))
+            return alert('شماره تلفن خود را به درستی وارد کنید!')
+
+        return setlogin({ stage: 'code' })
+    }
+
+    function CheckCode() {}
+
     return (
         <main class='login'>
             <iframe src='https://my.spline.design/untitled-51a258c8798e70e51855f9dd800ffa1c/'></iframe>
@@ -29,14 +51,32 @@ const Login: Component = props => {
                         شماره تلفن
                     </h3>
                     <input
-                        type='number'
-                        inputMode={'numeric'}
                         class='phone-inp title_small'
-                        placeholder='09123456789'
+                        type={'number'}
+                        inputmode={'numeric'}
+                        maxlength='11'
+                        classList={{ inpError: login.error !== '' }}
+                        placeholder='مثال: 09123456789'
+                        value={login.phone}
+                        oninput={e => {
+                            if (login.error) setlogin({ error: '' })
+
+                            return setlogin({
+                                phone: e.currentTarget.value,
+                            })
+                        }}
                     />
                 </div>
 
-                <button class='title_smaller cta'>ارسال کد</button>
+                <button
+                    class='title_smaller cta'
+                    onclick={() => {
+                        if (login.stage === 'phone') CheckPhone()
+                        else CheckCode()
+                    }}
+                >
+                    ارسال کد
+                </button>
             </div>
         </main>
     )
