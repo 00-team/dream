92057dbbec606e0f88fb042873ee30fb9575e265
007 <dr007cc@gmail.com>
2024-05-31 19:44:49 +0330

    fix(api): update products api

--- src/api/product.rs
+++ src/api/product.rs
@@ -2,7 +2,7 @@ use actix_web::web::Json;
 use actix_web::{get, Scope};
 use utoipa::OpenApi;
 
-use crate::config::{config, Product};
+use crate::config::{config, Product, Products};
 use crate::docs::UpdatePaths;
 use crate::models::Response;
 
@@ -19,12 +19,27 @@ pub struct ApiDoc;
 #[utoipa::path(
     get,
     responses(
-        (status = 200, body = Vec<Product>)
+        (status = 200, body = HashMap<String, Product>, example = json!({
+            "spotify": {
+                "logo": "/static/image/logo/spotify.png",
+                "data": ["username", "password"],
+                "color": "#1db954",
+                "image": "/static/image/banner/spotify.png",
+                "plans": {
+                    "family.1": [1200000, "خانواده ۱ ماهه"],
+                    "family.3": [3200000, "خانواده ۳ ماهه"],
+                    "family.6": [6200000, "خانواده ۶ ماهه"],
+                    "single.1": [6200000, "۱ ماهه"],
+                    "single.3": [6200000, "۳ ماهه"],
+                    "single.6": [6200000, "۶ ماهه"]
+                }
+            }
+        }))
     )
 )]
 /// Product List
 #[get("/")]
-async fn products() -> Response<Vec<Product>> {
+async fn products() -> Response<Products> {
     Ok(Json(config().products.clone()))
 }
 
