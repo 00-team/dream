dd0a2af0eeb9f3baece4e6d34d6ce3834f7a3b97
sadra <sadrataghavi1383@gmail.com>
2024-05-31 17:40:55 +0330

    feat(dashboard)

--- app/pages/dashboard/index.tsx
+++ app/pages/dashboard/index.tsx
@@ -1,4 +1,6 @@
-import { Component } from 'solid-js'
+import { UserIcon } from 'icons'
+import { Component, Show } from 'solid-js'
+import { self } from 'store/self'
 
 import './style/dashboard.scss'
 
@@ -10,7 +12,30 @@ const Dashboard: Component = props => {
     // })
     return (
         <main class='dashboard'>
-            <aside class='sidebar'></aside>
+            <aside class='sidebar'>
+                <div class='avatar'>
+                    <div class='profile-avatar'>
+                        <Show when={self.user.photo} fallback={<UserIcon />}>
+                            <img
+                                draggable={false}
+                                loading='lazy'
+                                decoding='async'
+                                src={`/record/${self.user.id}:${self.user.photo}`}
+                            />
+                        </Show>
+                    </div>
+                    <div class='name-avatar title_small'>
+                        <Show
+                            when={self.user.name}
+                            fallback={<span>خطا!</span>}
+                        >
+                            <h2>{self.user.name}</h2>
+                        </Show>
+                    </div>
+                </div>
+                <div class='links'></div>
+                <div class='logout'></div>
+            </aside>
             <aside class='wrapper'></aside>
         </main>
     )
