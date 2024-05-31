3f08f1ef3781cb61a298aef1491b39cc5e780958
sadra <sadrataghavi1383@gmail.com>
2024-05-31 16:38:12 +0330

    feat(dashboard): init

---
+++ app/pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'solid-js'
+
+import './style/dashboard.scss'
+
+const Dashboard: Component = props => {
+    return <main class='dashboard'></main>
+}
+
+export default Dashboard
