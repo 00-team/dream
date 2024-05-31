bac51b09cc8be779fabe9a2dbf9d5caaa5210e1b
sadra <sadrataghavi1383@gmail.com>
2024-05-31 16:58:07 +0330

    feat(dashboardd)

--- app/pages/dashboard/index.tsx
+++ app/pages/dashboard/index.tsx
@@ -3,7 +3,17 @@ import { Component } from 'solid-js'
 import './style/dashboard.scss'
 
 const Dashboard: Component = props => {
-    return <main class='dashboard'></main>
+    // const nav = useNavigate()
+
+    // createEffect(() => {
+    //     if (!self.loged_in) nav('/')
+    // })
+    return (
+        <main class='dashboard'>
+            <aside class='sidebar'></aside>
+            <aside class='wrapper'></aside>
+        </main>
+    )
 }
 
 export default Dashboard
