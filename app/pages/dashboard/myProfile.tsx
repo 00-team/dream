7bd83b3a398e69eb3f36a454b0b478f197b7bf61
sadra <sadrataghavi1383@gmail.com>
2024-06-01 14:20:29 +0330

    feat(profile)

--- app/pages/dashboard/myProfile.tsx
+++ app/pages/dashboard/myProfile.tsx
@@ -1,7 +1,41 @@
-import { Component } from 'solid-js'
+import { Special } from 'comps'
+import { Component, onMount } from 'solid-js'
+import { createStore } from 'solid-js/store'
+import { self } from 'store/self'
 
 import './style/profile.scss'
 
 export const MyProfile: Component = props => {
-    return <section class='profile'></section>
+    const [data, setdata] = createStore({
+        img: '',
+        name: '',
+    })
+
+    onMount(() => {
+        setdata({
+            img: self.user.photo,
+            name: self.user.name,
+        })
+    })
+
+    return (
+        <section class='profile'>
+            <div class='img-container'></div>
+            <div class='name-container'>
+                <input
+                    type='text'
+                    value={data.name}
+                    placeholder='اسم شما...'
+                    oninput={e => setdata({ name: e.currentTarget.value })}
+                    class='title_small'
+                    maxLength={256}
+                />
+            </div>
+            <Special
+                text='ذخیره'
+                class='save-cta'
+                onclick={() => alert('slm')}
+            />
+        </section>
+    )
 }
