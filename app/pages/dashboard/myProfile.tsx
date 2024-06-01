fed9e7c50b5512a05294f162fb69ed1320728aae
sadra <sadrataghavi1383@gmail.com>
2024-06-01 22:24:58 +0330

    feat(dashboard)

--- app/pages/dashboard/myProfile.tsx
+++ app/pages/dashboard/myProfile.tsx
@@ -1,4 +1,5 @@
 import { addAlert, Special } from 'comps'
+import { CrossIcon } from 'icons/home'
 import { httpx } from 'shared'
 import { Component, onMount } from 'solid-js'
 import { createStore } from 'solid-js/store'
@@ -74,43 +75,24 @@ export const MyProfile: Component = props => {
 
     return (
         <section class='profile'>
-            <label for='img-inp' class='img-container'>
-                <input
-                    type='file'
-                    id='img-inp'
-                    accept='.jpg, .jpeg, .png, image/jpg, image/jpeg, image/png'
-                    onchange={e => {
-                        if (!e.target.files || !e.target.files[0]) return
-
-                        const file = e.target.files[0]
-
-                        if (!IMAGE_MIMETYPE.includes(file.type))
-                            return addAlert({
-                                type: 'error',
-                                timeout: 5,
-                                content: 'فرمت واردی باید عکس باشد!',
-                                subject: 'خطا!',
-                            })
-
-                        const fd = new FormData()
-                        fd.set('photo', file)
-
-                        const url = URL.createObjectURL(file)
-
-                        setdata({
-                            img: url,
-                        })
-
+            <div class='img-wrapper'>
+                <div
+                    class='remove-img icon'
+                    role='button'
+                    onclick={() => {
                         httpx({
                             url: '/api/user/photo/',
-                            method: 'PUT',
-                            data: fd,
+                            method: 'DELETE',
                             onLoad(x) {
                                 if (x.status == 200) {
+                                    setdata({
+                                        img: '',
+                                    })
                                     setSelf({
-                                        loged_in: true,
-                                        fetch: false,
-                                        user: x.response,
+                                        user: {
+                                            ...self.user,
+                                            photo: '',
+                                        },
                                     })
                                     addAlert({
                                         type: 'success',
@@ -130,18 +112,80 @@ export const MyProfile: Component = props => {
                             },
                         })
                     }}
-                />
-                <img
-                    src={
-                        self.user.photo
-                            ? `/record/${self.user.id}:${self.user.photo}`
-                            : '/static/image/dashboard/default-avatar.webp'
-                    }
-                    draggable={false}
-                    class='img'
-                    alt=''
-                />
-            </label>
+                >
+                    <CrossIcon />
+                </div>
+                <label for='img-inp' class='img-container'>
+                    <input
+                        type='file'
+                        id='img-inp'
+                        accept='.jpg, .jpeg, .png, image/jpg, image/jpeg, image/png'
+                        onchange={e => {
+                            if (!e.target.files || !e.target.files[0]) return
+
+                            const file = e.target.files[0]
+
+                            if (!IMAGE_MIMETYPE.includes(file.type))
+                                return addAlert({
+                                    type: 'error',
+                                    timeout: 5,
+                                    content: 'فرمت واردی باید عکس باشد!',
+                                    subject: 'خطا!',
+                                })
+
+                            const fd = new FormData()
+                            fd.set('photo', file)
+
+                            const url = URL.createObjectURL(file)
+
+                            setdata({
+                                img: url,
+                            })
+
+                            httpx({
+                                url: '/api/user/photo/',
+                                method: 'PUT',
+                                data: fd,
+                                onLoad(x) {
+                                    if (x.status == 200) {
+                                        setSelf({
+                                            loged_in: true,
+                                            fetch: false,
+                                            user: x.response,
+                                        })
+                                        addAlert({
+                                            type: 'success',
+                                            timeout: 5,
+                                            content:
+                                                'عکس شما با موفقیت به روز شد',
+                                            subject: 'موفق!',
+                                        })
+                                    } else {
+                                        addAlert({
+                                            type: 'error',
+                                            timeout: 5,
+                                            content:
+                                                'مشکلی پیش امده کمی بعد دوباره تلاش کنید.',
+                                            subject: 'خطا!',
+                                        })
+                                    }
+                                },
+                            })
+                        }}
+                    />
+                    <img
+                        src={
+                            self.user.photo
+                                ? `/record/${self.user.id}:${self.user.photo}`
+                                : '/static/image/dashboard/default-avatar.webp'
+                        }
+                        draggable={false}
+                        class='img'
+                        alt=''
+                    />
+                </label>
+            </div>
+
             <div class='name-container'>
                 <input
                     type='text'
