aba0252da852a04ca5369f87ff7d967c9c080253
sadra <sadrataghavi1383@gmail.com>
2024-06-01 23:01:33 +0330

    feat(dashboard)

---
+++ app/pages/dashboard/myTransactions.tsx
@@ -0,0 +1,7 @@
+import { Component } from 'solid-js'
+
+import './style/transactions.scss'
+
+export const MyTransactions: Component = props => {
+    return <section class='transaction'></section>
+}
