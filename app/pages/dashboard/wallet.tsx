89e5c3da6f4390ec5b9bbf8ed4c133e235400406
sadra <sadrataghavi1383@gmail.com>
2024-06-02 20:56:06 +0330

    feat(wallet)

--- app/pages/dashboard/wallet.tsx
+++ app/pages/dashboard/wallet.tsx
@@ -1,7 +1,36 @@
+import { WalletIcon } from 'icons/dashboard'
 import { Component } from 'solid-js'
 
 import './style/wallet.scss'
 
+import logo from 'assets/image/logo.png'
+import bg from 'assets/image/card-bg.jpeg'
+import CountUp from 'comps/countUp'
+
 export const Wallet: Component = props => {
-    return <section class='wallet'></section>
+    return (
+        <section class='wallet'>
+            <div
+                class='wallet-current'
+                style={{
+                    'background-image': `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.6)),
+                    url(${bg})`,
+                }}
+            >
+                <div class='head'>
+                    <div class='holder title_smaller'>
+                        <WalletIcon />
+                        موجودی
+                    </div>
+                    <div class='logo'>
+                        <img src={logo} alt='' />
+                    </div>
+                </div>
+                <div class='center title_hero'>
+                    <CountUp steps={12345} addTime={20} end={1000000} format />
+                </div>
+                <div class='bottom'></div>
+            </div>
+        </section>
+    )
 }
