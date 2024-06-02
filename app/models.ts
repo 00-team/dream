3f2f1f0b2c7575b86e5b0afcb8d2e4a33ce67e7e
sadra <sadrataghavi1383@gmail.com>
2024-06-02 22:23:52 +0330

    feat(app): orders state

--- app/models.ts
+++ app/models.ts
@@ -7,6 +7,22 @@ export type ProductModel = {
     plans: { [k: string]: [number, string] }
 }
 
+export type OrdersType = {
+    amount: number
+    bank_track_id: number
+    card: string
+    date: number
+    hashed_card: string
+    id: number
+    kind: 'in' | 'out'
+    status: 'in_progress' | 'failed' | 'success'
+    timestamp: number
+    user: number
+    vendor: 'zarinpal' | 'zibal'
+    vendor_order_id: string
+    vendor_track_id: number
+}
+
 export type UserModel = {
     id: number
     admin: boolean
