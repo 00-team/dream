ffd8d69ef067d1a98a968a12925239fa79394a6e
sadra <sadrataghavi1383@gmail.com>
2024-06-02 18:55:20 +0330

    refactor(dashboard): file names change

--- app/pages/dashboard/myTransactions.tsx
+++
@@ -1,7 +0,0 @@
-import { Component } from 'solid-js'
-
-import './style/transactions.scss'
-
-export const MyTransactions: Component = props => {
-    return <section class='transaction'></section>
-}
--- app/pages/dashboard/myWallet.tsx
+++
@@ -1,7 +0,0 @@
-import { Component } from 'solid-js'
-
-import './style/wallet.scss'
-
-export const MyWallet: Component = props => {
-    return <section class='wallet'></section>
-}
