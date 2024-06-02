37b18dfb2db0a6303a98f08e83f36a70e8154779
sadra <sadrataghavi1383@gmail.com>
2024-06-02 22:24:10 +0330

    feat(wallet)

--- app/pages/dashboard/wallet.tsx
+++ app/pages/dashboard/wallet.tsx
@@ -1,11 +1,15 @@
 import { WalletIcon } from 'icons/dashboard'
-import { Component } from 'solid-js'
+import { Component, onMount } from 'solid-js'
 
 import './style/wallet.scss'
 
-import logo from 'assets/image/logo.png'
 import bg from 'assets/image/card-bg.jpeg'
+import logo from 'assets/image/logo.png'
+import { addAlert } from 'comps'
 import CountUp from 'comps/countUp'
+import { OrdersType } from 'models'
+import { httpx } from 'shared'
+import { createStore } from 'solid-js/store'
 import { self } from 'store/self'
 
 export const Wallet: Component = props => {
@@ -35,6 +39,68 @@ export const Wallet: Component = props => {
                     <span class='title_small'>{self.user.name}</span>
                 </div>
             </div>
+
+            <Transactions />
         </section>
+    )
+}
+
+type stateType = {
+    orders: OrdersType[]
+}
+
+const Transactions: Component = P => {
+    const [state, setstate] = createStore<stateType>({
+        orders: [],
+    })
+
+    onMount(() => {
+        httpx({
+            url: '/api/user/transactions/',
+            method: 'GET',
+            onLoad(x) {
+                if (x.status == 200) {
+                    setstate({
+                        orders: x.response,
+                    })
+                } else {
+                    addAlert({
+                        type: 'error',
+                        timeout: 5,
+                        content: 'مشکلی پیش امده کمی بعد دوباره تلاش کنید.',
+                        subject: 'خطا!',
+                    })
+                }
+            },
+        })
+    })
+
+    return (
+        <div class='transactions'>
+            {state.orders.length >= 1 ? (
+                <table>
+                    <thead class='title_small'>
+                        <tr>
+                            <th class='id'>شماره</th>
+                            <th class='kind'>نوع</th>
+                            <th class='vendor'>سرویس</th>
+                            <th class='amount'>مقدار</th>
+                            <th class='date'>تاریخ</th>
+                        </tr>
+                    </thead>
+                    <tbody class='title_smaller'>
+                        <tr>
+                            <td>1</td>
+                            <td>یسشیش</td>
+                            <td>یسشیشس</td>
+                            <td>09120945</td>
+                            <td>یسشیش</td>
+                        </tr>
+                    </tbody>
+                </table>
+            ) : (
+                <div class='no-orders title_hero'>شما سفارشی ندارید :(</div>
+            )}
+        </div>
     )
 }
