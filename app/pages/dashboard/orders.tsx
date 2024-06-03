d3288df98239426786bec9024c07349bb9481005
sadra <sadrataghavi1383@gmail.com>
2024-06-03 12:26:04 +0330

    feat(orders)

--- app/pages/dashboard/orders.tsx
+++ app/pages/dashboard/orders.tsx
@@ -22,9 +22,33 @@ export const Orders: Component = props => {
                     {/* <div class='text-wrapper'>
                         شما سفارش ثبت شده ای ندارید! :(
                     </div> */}
-                    <div class='orders-example'></div>
+                    <div class='orders-example'>
+                        <OrderExample />
+                    </div>
                 </div>
             )}
         </section>
+    )
+}
+
+const OrderExample: Component = P => {
+    return (
+        <div class='order'>
+            <div class='kind'>
+                <img src='/static/image/logo/spotify.png' alt='' />
+                <div class='holder column'>
+                    <div class='label title_smaller'>نوع سفارش</div>
+                    <div class='data title'>spotify</div>
+                </div>
+            </div>
+            <div class='price column'>
+                <div class='label title_smaller'>قیمت پرداختی</div>
+                <div class='data title'>100,000 تومان</div>
+            </div>
+            <div class='status column'>
+                <div class='label title_smaller'>وضعیت </div>
+                <div class='data title done'>تحویل داده شده </div>
+            </div>
+        </div>
     )
 }
