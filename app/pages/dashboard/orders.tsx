f369776501e77c778f7e5c12d82ded9a3569e3b0
sadra <sadrataghavi1383@gmail.com>
2024-06-03 12:03:51 +0330

    feat(dashboard)

--- app/pages/dashboard/orders.tsx
+++ app/pages/dashboard/orders.tsx
@@ -1,7 +1,30 @@
+import { OrderType } from 'models'
 import { Component } from 'solid-js'
+import { createStore } from 'solid-js/store'
 
 import './style/orders.scss'
 
+type stateType = {
+    orders: OrderType[]
+}
+
 export const Orders: Component = props => {
-    return <section class='orders'></section>
+    const [state, setstate] = createStore<stateType>({
+        orders: [],
+    })
+
+    return (
+        <section class='orders'>
+            {state.orders.length >= 1 ? (
+                <></>
+            ) : (
+                <div class='no-orders title_hero'>
+                    {/* <div class='text-wrapper'>
+                        شما سفارش ثبت شده ای ندارید! :(
+                    </div> */}
+                    <div class='orders-example'></div>
+                </div>
+            )}
+        </section>
+    )
 }
