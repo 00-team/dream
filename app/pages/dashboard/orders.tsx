e73486ae5733992cc41e65a9a21277cc960cfdca
sadra <sadrataghavi1383@gmail.com>
2024-06-03 13:37:12 +0330

    feat(orders)

--- app/pages/dashboard/orders.tsx
+++ app/pages/dashboard/orders.tsx
@@ -1,6 +1,8 @@
+import { addAlert } from 'comps'
 import { ArrowDownIcon } from 'icons/home'
 import { OrderType } from 'models'
-import { Component, createSignal } from 'solid-js'
+import { httpx } from 'shared'
+import { Component, createSignal, onMount } from 'solid-js'
 import { createStore } from 'solid-js/store'
 
 import './style/orders.scss'
@@ -14,11 +16,33 @@ export const Orders: Component = props => {
         orders: [],
     })
 
+    onMount(() => {
+        httpx({
+            url: '/api/orders/',
+            method: 'GET',
+            params: { page: 0 },
+            onLoad(x) {
+                if (x.status == 200) {
+                    setstate({ orders: x.response })
+                } else {
+                    addAlert({
+                        type: 'error',
+                        timeout: 5,
+                        content: 'مشکلی پیش امده کمی بعد دوباره تلاش کنید.',
+                        subject: 'خطا!',
+                    })
+                }
+            },
+        })
+    })
+
     return (
         <section class='orders'>
-            {state.orders.length <= 1 ? (
+            {state.orders.length >= 1 ? (
                 <div class='orders-wrapper'>
-                    <Order />
+                    {state.orders.map(order => (
+                        <Order {...order} />
+                    ))}
                 </div>
             ) : (
                 <div class='no-orders title_hero'>
@@ -98,8 +122,14 @@ const OrderExample: Component<OrderExample> = P => {
     )
 }
 
-const Order: Component = P => {
+const Order: Component<OrderType> = P => {
     const [active, setactive] = createSignal(false)
+
+    const getTime = (timestamp: number) => {
+        let offset = Math.abs(new Date().getTimezoneOffset()) * 60
+
+        return (timestamp + offset) * 1000
+    }
 
     return (
         <div
@@ -112,27 +142,43 @@ const Order: Component = P => {
             </div>
             <div class='upper'>
                 <div class='kind'>
-                    <img
-                        loading='lazy'
-                        decoding='async'
-                        src={'/static/image/logo/spotify.png'}
-                        alt=''
-                    />
+                    <img loading='lazy' decoding='async' src={P.img} alt='' />
                     <div class='holder column'>
                         <div class='label title_smaller'>نوع سفارش</div>
-                        <div class='data title'>spotify</div>
+                        <div class='data title'>{P.kind}</div>
                     </div>
                 </div>
                 <div class='price column'>
                     <div class='label title_smaller'>قیمت پرداختی</div>
-                    <div class='data title'>100000 تومان</div>
+                    <div class='data title'>{P.price} تومان</div>
                 </div>
                 <div class='status column'>
                     <div class='label title_smaller'>وضعیت </div>
-                    <div class={`data title done`}>تمام شده</div>
+                    <div class={`data title done`}>{P.status} </div>
                 </div>
             </div>
-            <div class='lower'></div>
+            <div class='lower'>
+                <div class='id column'>
+                    <div class='label title_smaller'>شماره سفارش </div>
+                    <div class='data title'>{P.id}</div>
+                </div>
+                <div class='price column'>
+                    <div class='label title_smaller'>توضیحات</div>
+                    <div class='data title'>email: elan@dsaft.com</div>
+                    <div class='data title'>password: sadsdaweesa</div>
+                    <div class='data title_smaller order-desc'>
+                        لورمزیانمزن انشیاسنمشای نمشاسینا شنمیانمسشا
+                    </div>
+                </div>
+                <div class='status column'>
+                    <div class='label title_smaller'>تاریخ ثبت</div>
+                    <div class={`data title done`}>
+                        {new Date(getTime(P.timestamp)).toLocaleDateString(
+                            'fa-IR'
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
