b8f3bbd300f5fe72898e7d4f1e91ec3f11fafd9c
sadra <sadrataghavi1383@gmail.com>
2024-06-03 12:37:06 +0330

    feat(orders)

--- app/pages/dashboard/orders.tsx
+++ app/pages/dashboard/orders.tsx
@@ -19,11 +19,46 @@ export const Orders: Component = props => {
                 <></>
             ) : (
                 <div class='no-orders title_hero'>
-                    {/* <div class='text-wrapper'>
+                    <div class='text-wrapper'>
                         شما سفارش ثبت شده ای ندارید! :(
-                    </div> */}
+                    </div>
                     <div class='orders-example'>
-                        <OrderExample />
+                        <OrderExample
+                            img='/static/image/logo/spotify.png'
+                            kind='spotify'
+                            price={100000}
+                            status='done'
+                        />
+                        <OrderExample
+                            img='/static/image/logo/netflix.jpg'
+                            kind='netflix'
+                            price={20000}
+                            status='refunded'
+                        />
+                        <OrderExample
+                            img='/static/image/logo/discord.png'
+                            kind='discord'
+                            price={600000}
+                            status='waiting'
+                        />
+                        <OrderExample
+                            img='/static/image/logo/canva.png'
+                            kind='canva'
+                            price={400000}
+                            status='done'
+                        />
+                        <OrderExample
+                            img='/static/image/logo/prime.png'
+                            kind='prime'
+                            price={50000}
+                            status='refunded'
+                        />
+                        <OrderExample
+                            img='/static/image/logo/apple-music.png'
+                            kind='apple music'
+                            price={900000}
+                            status='waiting'
+                        />
                     </div>
                 </div>
             )}
@@ -31,23 +66,30 @@ export const Orders: Component = props => {
     )
 }
 
-const OrderExample: Component = P => {
+interface OrderExample {
+    img: string
+    kind: OrderType['kind']
+    price: OrderType['price']
+    status: OrderType['status']
+}
+
+const OrderExample: Component<OrderExample> = P => {
     return (
         <div class='order'>
             <div class='kind'>
-                <img src='/static/image/logo/spotify.png' alt='' />
+                <img loading='lazy' decoding='async' src={P.img} alt='' />
                 <div class='holder column'>
                     <div class='label title_smaller'>نوع سفارش</div>
-                    <div class='data title'>spotify</div>
+                    <div class='data title'>{P.kind}</div>
                 </div>
             </div>
             <div class='price column'>
                 <div class='label title_smaller'>قیمت پرداختی</div>
-                <div class='data title'>100,000 تومان</div>
+                <div class='data title'>{P.price} تومان</div>
             </div>
             <div class='status column'>
                 <div class='label title_smaller'>وضعیت </div>
-                <div class='data title done'>تحویل داده شده </div>
+                <div class={`data title ${P.status}`}>{P.status}</div>
             </div>
         </div>
     )
