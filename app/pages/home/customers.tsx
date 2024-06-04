6bc4d6361e2e09455c72a7085134fbb0ffd64021
sadra <sadrataghavi1383@gmail.com>
2024-06-04 21:18:47 +0330

    feat(customers):

--- app/pages/home/customers.tsx
+++ app/pages/home/customers.tsx
@@ -1,4 +1,4 @@
-import { Component, onMount } from 'solid-js'
+import { Component, onCleanup, onMount } from 'solid-js'
 
 import './style/customers.scss'
 
@@ -61,6 +61,10 @@ export const Customers: Component = props => {
                 lastScrollPosition = currentScrollPosition
             }
         }
+    })
+
+    onCleanup(() => {
+        audio = null
     })
 
     function typeMessage() {
