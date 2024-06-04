1d2eb30566604e237efe60a29c3c03882fbfd530
007 <dr007cc@gmail.com>
2024-06-04 20:36:21 +0330

    feat(app): add 00 Team signature

--- app/layout/footer.tsx
+++ app/layout/footer.tsx
@@ -1,111 +1,118 @@
-import { Component } from 'solid-js'
-
 import './style/footer.scss'
 
 import { InstaIcon, TelegramIcon, WhatsappIcon } from 'icons/footer'
 import logo from 'assets/image/logo.png'
+import Signature from './signature'
 
-const Footer: Component = props => {
+export default () => {
     return (
-        <footer class='footer-container'>
-            <div class='footer-wrapper'>
-                <div class='footer-dream'>
-                    <h4 class='footer-title section_title'>
-                        <img src={logo} alt='' />
-                        Dream Pay{' '}
-                    </h4>
-                    <p class='footer-description title_smaller'>
-                        با دیریم پی، بهترین سرویس‌های اکانت اسپاتیفای، اپل
-                        موزیک، دیسکورد و ... را تجربه کن! ارائه‌دهنده بهترین
-                        اکانت‌های محبوب اینترنت با کیفیت برتر و قیمت مناسب.
-                        انتخاب بهتر، سرعت بیشتر! با اعتماد به دیریم پی، مشتریان
-                        دسترسی آسان و امن به اکانت‌های مورد نیاز خود را خواهند
-                        داشت و از امکانات ویژه‌ی هر یک از سرویس‌ها بهره‌مند
-                        خواهند شد.
-                    </p>
-                </div>
-
-                <div class='footer-links'>
-                    <div class='dream-socials section_title'>
-                        <a
-                            class='social icon telegram'
-                            href='https://t.me/heydaricoir'
-                        >
-                            <TelegramIcon />
-                        </a>
-
-                        <a
-                            class='social icon whatsapp'
-                            href="'https://wa.me/+989129429430'"
-                        >
-                            <WhatsappIcon />
-                        </a>
-
-                        <a
-                            class='social icon instagram'
-                            href='instagram://user?username=heydari.chair'
-                        >
-                            <InstaIcon />
-                        </a>
+        <footer class='footer-fnd'>
+            <div class='footer-container'>
+                <div class='footer-wrapper'>
+                    <div class='footer-dream'>
+                        <h4 class='footer-title section_title'>
+                            <img src={logo} alt='' />
+                            Dream Pay{' '}
+                        </h4>
+                        <p class='footer-description title_smaller'>
+                            با دیریم پی، بهترین سرویس‌های اکانت اسپاتیفای، اپل
+                            موزیک، دیسکورد و ... را تجربه کن! ارائه‌دهنده بهترین
+                            اکانت‌های محبوب اینترنت با کیفیت برتر و قیمت مناسب.
+                            انتخاب بهتر، سرعت بیشتر! با اعتماد به دیریم پی،
+                            مشتریان دسترسی آسان و امن به اکانت‌های مورد نیاز خود
+                            را خواهند داشت و از امکانات ویژه‌ی هر یک از سرویس‌ها
+                            بهره‌مند خواهند شد.
+                        </p>
                     </div>
-                    <div class='site-links'>
-                        <div class='link-column'>
-                            <h4 class='column-head title'>دسترسی سریع</h4>
-                            <a href='/#' class='title_smaller column-link'>
-                                خانه
-                            </a>
-                            <a href='/#about' class='title_smaller column-link'>
-                                درباره ما
-                            </a>
+
+                    <div class='footer-links'>
+                        <div class='dream-socials section_title'>
                             <a
-                                href='/products'
-                                class='title_smaller column-link'
+                                class='social icon telegram'
+                                href='https://t.me/heydaricoir'
                             >
-                                محصولات
+                                <TelegramIcon />
                             </a>
+
                             <a
-                                href='/#customers'
-                                class='title_smaller column-link'
+                                class='social icon whatsapp'
+                                href="'https://wa.me/+989129429430'"
                             >
-                                نظرات مشتری ها
+                                <WhatsappIcon />
                             </a>
+
                             <a
-                                href='/#contact'
-                                class='title_smaller column-link'
+                                class='social icon instagram'
+                                href='instagram://user?username=heydari.chair'
                             >
-                                ارتباط با ما
-                            </a>
-                            <a href='/#faq' class='title_smaller column-link'>
-                                سوالات متداول
+                                <InstaIcon />
                             </a>
                         </div>
-                        <div class='link-column'>
-                            <h4 class='column-head title'>اطلاعات بیشتر</h4>
-                            <a href='/blogs' class='title_smaller column-link'>
-                                مقالات
-                            </a>
-                            <a href='/' class='title_smaller column-link'>
-                                همکاری
-                            </a>
-                            <a href='/' class='title_smaller column-link'>
-                                حریم خصوصی
-                            </a>
-                            <a href='/' class='title_smaller column-link'>
-                                شرایط استفاده
-                            </a>
+                        <div class='site-links'>
+                            <div class='link-column'>
+                                <h4 class='column-head title'>دسترسی سریع</h4>
+                                <a href='/#' class='title_smaller column-link'>
+                                    خانه
+                                </a>
+                                <a
+                                    href='/#about'
+                                    class='title_smaller column-link'
+                                >
+                                    درباره ما
+                                </a>
+                                <a
+                                    href='/products'
+                                    class='title_smaller column-link'
+                                >
+                                    محصولات
+                                </a>
+                                <a
+                                    href='/#customers'
+                                    class='title_smaller column-link'
+                                >
+                                    نظرات مشتری ها
+                                </a>
+                                <a
+                                    href='/#contact'
+                                    class='title_smaller column-link'
+                                >
+                                    ارتباط با ما
+                                </a>
+                                <a
+                                    href='/#faq'
+                                    class='title_smaller column-link'
+                                >
+                                    سوالات متداول
+                                </a>
+                            </div>
+                            <div class='link-column'>
+                                <h4 class='column-head title'>اطلاعات بیشتر</h4>
+                                <a
+                                    href='/blogs'
+                                    class='title_smaller column-link'
+                                >
+                                    مقالات
+                                </a>
+                                <a href='/' class='title_smaller column-link'>
+                                    همکاری
+                                </a>
+                                <a href='/' class='title_smaller column-link'>
+                                    حریم خصوصی
+                                </a>
+                                <a href='/' class='title_smaller column-link'>
+                                    شرایط استفاده
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class='en title_smaller'>
-                © 2024 Dream Pay. All rights reserved.
+
+                <div class='en title_smaller'>
+                    © 2024 Dream Pay. All rights reserved.
+                </div>
             </div>
 
-            <div class='footer-00team title_smaller'>
-                <div>developed by 00 team</div>
-            </div>
+            <Signature />
         </footer>
     )
 }
-
-export default Footer
---
+++ app/layout/signature/index.tsx
@@ -0,0 +1,14 @@
+import './style.scss'
+
+export default () => {
+    return (
+        <a
+            class='signature-00-team'
+            href='https://00-team.org'
+            draggable={false}
+            target='_blank'
+        >
+            developed by 00 team
+        </a>
+    )
+}
