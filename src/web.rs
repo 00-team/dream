70e20622e012241345e033617e1def57f2d57e22
007 <dr007cc@gmail.com>
2024-06-06 04:19:50 +0330

    feat(api): add all routes

--- src/web.rs
+++ src/web.rs
@@ -1,20 +1,25 @@
 use actix_web::dev::HttpServiceFactory;
 use actix_web::http::header::ContentType;
 use actix_web::middleware::NormalizePath;
-use actix_web::{get, routes, HttpResponse, Scope};
+use actix_web::{routes, HttpResponse, Scope};
 use std::fs::read_to_string;
 
 #[routes]
 #[get("/")]
 #[get("/login")]
 #[get("/products")]
+#[get("/dashboard")]
+#[get("/dashboard/wallet")]
+#[get("/dashboard/orders")]
 async fn app_index() -> HttpResponse {
     let result = read_to_string("app/dist/index.html")
         .unwrap_or("err reading app index.html".to_string());
     HttpResponse::Ok().content_type(ContentType::html()).body(result)
 }
 
+#[routes]
 #[get("/admin")]
+#[get("/admin/orders")]
 async fn admin_index() -> HttpResponse {
     let result = read_to_string("admin/dist/index.html")
         .unwrap_or("err reading admin index.html".to_string());
