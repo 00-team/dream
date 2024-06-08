cce9a8411ad730e20d3da136f39886c4acaf2334
007 <dr007cc@gmail.com>
2024-06-08 06:12:45 +0330

    feat(admin): update NewDiscount Form

--- admin/layout/discounts.tsx
+++ admin/layout/discounts.tsx
@@ -1,17 +1,21 @@
 import { useSearchParams } from '@solidjs/router'
 import './style/discounts.scss'
 import { createStore } from 'solid-js/store'
-import { Component, Show, createEffect } from 'solid-js'
-import { fmt_mdhm, httpx } from 'shared'
+import { Component, Match, Show, createEffect, createMemo } from 'solid-js'
+import { fmt_mdhm, fmt_parse_seconds, httpx } from 'shared'
 import { DiscountModel } from 'models'
 import { Copiable } from 'comps'
 import {
+    ChevronDownIcon,
     ChevronLeftIcon,
     ChevronRightIcon,
+    ChevronUpIcon,
     HourglassIcon,
+    PlusIcon,
     ShieldCheckIcon,
     ShieldXIcon,
     WrenchIcon,
+    XIcon,
 } from 'icons'
 import { Select } from 'comps'
 
@@ -47,7 +51,7 @@ export default () => {
     return (
         <div class='discounts-fnd'>
             <div class='discount-list'>
-                <NewDiscount />
+                <NewDiscount onNew={() => fetch_discounts(0)} />
                 {state.discounts.map(d => (
                     <Discount discount={d} />
                 ))}
@@ -122,28 +126,258 @@ const Discount: Component<DiscountProps> = P => {
     )
 }
 
-const NewDiscount = () => {
+type NewDiscountProps = {
+    onNew(): void
+}
+const NewDiscount: Component<NewDiscountProps> = P => {
+    type State = Omit<DiscountModel, 'id' | 'uses' | 'disabled' | 'expires'> & {
+        new_discount: boolean
+        set_expires: boolean
+        expires: {
+            months: number
+            days: number
+            hours: number
+            minutes: number
+        }
+    }
+    const [state, setState] = createStore<State>({
+        new_discount: true,
+        code: '',
+        amount: 0,
+        max_uses: null,
+        set_expires: false,
+        expires: {
+            months: 0,
+            days: 0,
+            hours: 0,
+            minutes: 0,
+        },
+        kind: null,
+        plan: null,
+    })
+
+    function add() {
+        let expires: number | null = null
+        if (state.set_expires) {
+            expires =
+                state.expires.months * 2592000 +
+                state.expires.days * 86400 +
+                state.expires.hours * 3600 +
+                state.expires.minutes * 60
+        }
+
+        httpx({
+            url: '/api/admin/discounts/',
+            method: 'POST',
+            json: {
+                code: state.code,
+                amount: state.amount,
+                kind: state.kind,
+                plan: state.plan,
+                expires: expires,
+                max_uses: state.max_uses,
+            },
+            onLoad(x) {
+                if (x.status != 200) return
+                P.onNew()
+            },
+        })
+    }
+
     return (
         <div class='new-discount'>
-            <span>Code: </span>
-            <input maxLength={255} class='styled' placeholder='code' />
-            <span>Amount: </span>
-            <input type='number' max={100} class='styled' placeholder='69%' />
-            <span>kind: </span>
-            <Select
-                items={[{ display: 'hi', idx: 0 }]}
-                multiple
-                onChange={v => console.log(v)}
-            />
-            <span>plan: </span>
-            <Select
-                items={[{ display: 'hi', idx: 0 }]}
-                onChange={v => console.log(v)}
-            />
-            <span>Expires: </span>
-            <input type='number' class='styled' placeholder='in seconds' />
-            <span>Max Uses: </span>
-            <input type='number' class='styled' placeholder='420' />
+            <div class='top'>
+                <span>New Discount</span>
+                <div class='actions'>
+                    <Show when={state.new_discount && state.code}>
+                        <button class='add-btn styled icon' onClick={add}>
+                            <PlusIcon />
+                        </button>
+                    </Show>
+                    <button
+                        class='styled icon'
+                        onClick={() =>
+                            setState(s => ({ new_discount: !s.new_discount }))
+                        }
+                    >
+                        <Show
+                            when={state.new_discount}
+                            fallback={<ChevronDownIcon />}
+                        >
+                            <ChevronUpIcon />
+                        </Show>
+                    </button>
+                </div>
+            </div>
+            <Show when={state.new_discount}>
+                <div class='bottom'>
+                    <span>Code: </span>
+                    <input
+                        maxLength={255}
+                        class='styled'
+                        placeholder='code'
+                        value={state.code}
+                        onInput={e => {
+                            let code = e.currentTarget.value.slice(0, 255)
+                            e.currentTarget.value = code
+                            setState({ code })
+                        }}
+                    />
+                    <span>Amount: </span>
+                    <input
+                        type='number'
+                        max={100}
+                        min={0}
+                        class='styled'
+                        placeholder='69%'
+                        value={state.amount}
+                        onInput={e => {
+                            let amount = parseInt(e.currentTarget.value) || 0
+                            amount = Math.max(Math.min(amount, 100), 0)
+                            e.currentTarget.value = amount.toString()
+                            setState({ amount })
+                        }}
+                    />
+                    <span>kind: </span>
+                    <Select
+                        items={[{ display: 'hi', idx: 0 }]}
+                        multiple
+                        onChange={v => console.log(v)}
+                    />
+                    <span>plan: </span>
+                    <Select
+                        items={[{ display: 'hi', idx: 0 }]}
+                        onChange={v => console.log(v)}
+                    />
+                    <span>Expires: </span>
+                    <div class='row expires'>
+                        <button
+                            class='styled icon remove'
+                            onClick={() =>
+                                setState({
+                                    set_expires: false,
+                                    expires: {
+                                        months: 0,
+                                        days: 0,
+                                        hours: 0,
+                                        minutes: 0,
+                                    },
+                                })
+                            }
+                        >
+                            <XIcon />
+                        </button>
+                        <input
+                            type='number'
+                            class='styled'
+                            placeholder='Months'
+                            value={state.expires.months}
+                            min={0}
+                            max={1600}
+                            onInput={e => {
+                                let v = parseInt(e.currentTarget.value) || 0
+                                v = Math.max(Math.min(v, 1600), 0)
+                                e.currentTarget.value = v.toString()
+                                setState(s => ({
+                                    expires: { ...s.expires, months: v },
+                                    set_expires: true,
+                                }))
+                            }}
+                        />
+                        <input
+                            type='number'
+                            class='styled'
+                            placeholder='Days'
+                            value={state.expires.days}
+                            min={0}
+                            max={30}
+                            onInput={e => {
+                                let v = parseInt(e.currentTarget.value) || 0
+                                v = Math.max(Math.min(v, 30), 0)
+                                e.currentTarget.value = v.toString()
+                                setState(s => ({
+                                    expires: { ...s.expires, days: v },
+                                    set_expires: true,
+                                }))
+                            }}
+                        />
+                        <input
+                            type='number'
+                            class='styled'
+                            placeholder='Hours'
+                            value={state.expires.hours}
+                            min={0}
+                            max={24}
+                            onInput={e => {
+                                let v = parseInt(e.currentTarget.value) || 0
+                                v = Math.max(Math.min(v, 24), 0)
+                                e.currentTarget.value = v.toString()
+                                setState(s => ({
+                                    expires: { ...s.expires, hours: v },
+                                    set_expires: true,
+                                }))
+                            }}
+                        />
+                        <input
+                            type='number'
+                            class='styled'
+                            placeholder='Hours'
+                            value={state.expires.minutes}
+                            min={0}
+                            max={60}
+                            onInput={e => {
+                                let v = parseInt(e.currentTarget.value) || 0
+                                v = Math.max(Math.min(v, 60), 0)
+                                e.currentTarget.value = v.toString()
+                                setState(s => ({
+                                    expires: { ...s.expires, minutes: v },
+                                    set_expires: true,
+                                }))
+                            }}
+                        />
+                        <Show when={state.set_expires} fallback='(null)'>
+                            <span>
+                                Months {state.expires.months} & Days{' '}
+                                {state.expires.days} &{' '}
+                                {state.expires.hours
+                                    .toString()
+                                    .padStart(2, '0')}
+                                :
+                                {state.expires.minutes
+                                    .toString()
+                                    .padStart(2, '0')}
+                            </span>
+                        </Show>
+                    </div>
+                    <span>Max Uses: </span>
+                    <div class='row'>
+                        <button
+                            class='styled icon remove'
+                            onClick={() => setState({ max_uses: null })}
+                        >
+                            <XIcon />
+                        </button>
+                        <input
+                            type='number'
+                            class='styled'
+                            min={0}
+                            max={4294967295}
+                            placeholder='420'
+                            value={state.max_uses}
+                            onInput={e => {
+                                if (!e.currentTarget.value) {
+                                    setState({ max_uses: null })
+                                    return
+                                }
+                                let v = parseInt(e.currentTarget.value) || 0
+                                v = Math.max(Math.min(v, 4294967295), 0)
+                                e.currentTarget.value = v.toString()
+                                setState({ max_uses: v })
+                            }}
+                        />
+                    </div>
+                </div>
+            </Show>
         </div>
     )
 }
--- admin/shared/fmt.ts
+++ admin/shared/fmt.ts
@@ -2,21 +2,33 @@ function pad0(v: string | number): string {
     return v.toString().padStart(2, '0')
 }
 
+type ParsedSeconds = {
+    months: number
+    days: number
+    hours: number
+    minutes: number
+}
+export function fmt_parse_seconds(seconds: number): ParsedSeconds {
+    let months = ~~(seconds / 2592000)
+    let O = months * 2592000
+    let days = ~~((seconds - O) / 86400)
+    O = O + days * 86400
+    let hours = ~~((seconds - O) / 3600)
+    O = O + hours * 3600
+    let minutes = ~~((seconds - O) / 60)
+
+    return {
+        months,
+        days,
+        hours,
+        minutes,
+    }
+}
+
 export function fmt_mdhm(ts: number): string {
     let now = new Date().getTime() / 1e3
     let expires = ts - now
     if (expires < 0) return 'Expired'
-
-    let months = ~~(expires / 2592000)
-    let O = months * 2592000
-    let days = ~~((expires - O) / 86400)
-    O = O + days * 86400
-    let hours = ~~((expires - O) / 3600)
-    O = O + hours * 3600
-    let minutes = ~~((expires - O) / 60)
-
-    let out = `${days} Days & ${pad0(hours)}:${pad0(minutes)}`
-    if (months) out = months + ' Months & ' + out
-
-    return out
+    let { months, days, hours, minutes } = fmt_parse_seconds(expires)
+    return `${months} Months & ${days} Days & ${pad0(hours)}:${pad0(minutes)}`
 }
