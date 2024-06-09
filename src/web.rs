c1c3382813ea30bd0f50e421d184207bc3d92e90
007 <dr007cc@gmail.com>
2024-06-09 19:38:18 +0330

    feat: add enamad code

--- src/web.rs
+++ src/web.rs
@@ -1,7 +1,7 @@
 use actix_web::dev::HttpServiceFactory;
 use actix_web::http::header::ContentType;
 use actix_web::middleware::NormalizePath;
-use actix_web::{routes, HttpResponse, Scope};
+use actix_web::{get, routes, HttpResponse, Scope};
 use std::fs::read_to_string;
 
 #[routes]
@@ -15,6 +15,11 @@ async fn app_index() -> HttpResponse {
     let result = read_to_string("app/dist/index.html")
         .unwrap_or("err reading app index.html".to_string());
     HttpResponse::Ok().content_type(ContentType::html()).body(result)
+}
+
+#[get("/6025084.txt")]
+async fn enamad_code() -> HttpResponse {
+    HttpResponse::Ok().content_type(ContentType::plaintext()).body("6025084")
 }
 
 #[routes]
@@ -32,4 +37,5 @@ pub fn router() -> impl HttpServiceFactory {
         .wrap(NormalizePath::trim())
         .service(app_index)
         .service(admin_index)
+        .service(enamad_code)
 }
